9a951b57c5060b42402c8563a0c545dcd46e5e3e
yungo1846 <yungo1468@naver.com>
2021-02-03 20:29:55 +0900

    feat: 더하기 기능 추가

--- src/js/index.js
+++ src/js/index.js
@@ -6,43 +6,70 @@ class Calculator {
     //   modifiers: document.querySelector('.modifiers'),
     //   operations: document.querySelector('.operations'),
     // };
-
+    this.operands = [];
+    this.operator = "";
+    this.isTimeToInputNumber = false;
     this.setEventListener();
   }
 
   setEventListener() {
-    const $digits = document.querySelector('.digits');
-    const $modifiers = document.querySelector('.modifiers');
-    const $total = document.querySelector('#total');
+    const $digits = document.querySelector(".digits");
+    const $modifiers = document.querySelector(".modifiers");
+    const $operations = document.querySelector(".operations");
+    const $total = document.querySelector("#total");
 
-    $digits.addEventListener('click', (event) => {
-      if (event.target.className !== 'digit') return;
+    $digits.addEventListener("click", (event) => {
+      if (event.target.className !== "digit") return;
 
       const digit = event.target.innerText;
 
-      if ($total.innerText.length >= 3) return;
+      if ($total.innerText.length >= 3 && !this.isTimeToInputNumber) return;
 
-      if ($total.innerText === '0') {
-        $total.innerText = '';
+      if ($total.innerText === "0" || this.isTimeToInputNumber) {
+        $total.innerText = "";
+        this.isTimeToInputNumber = false;
       }
 
       $total.innerText += digit;
     });
 
-    $modifiers.addEventListener('click', (event) => {
-      if (event.target.className !== 'modifier') return;
+    $modifiers.addEventListener("click", (event) => {
+      if (event.target.className !== "modifier") return;
 
       const modifier = event.target.innerText;
 
-      if (modifier === 'AC') {
+      if (modifier === "AC") {
         this.allClear();
       }
+    });
+
+    $operations.addEventListener("click", (event) => {
+      if (event.target.className !== "operation") return;
+
+      const operation = event.target.innerText;
+      this.operands.push(Number($total.innerText));
+
+      if (operation !== "=") {
+        this.operator = operation;
+        this.isTimeToInputNumber = true;
+        return;
+      }
+
+      let result = 0;
+      if (this.operator === "+") {
+        result = this.operands[0] + this.operands[1];
+      }
+      console.log(this.operands[0] + this.operands[1]);
+
+      $total.innerText = result;
     });
   }
 
   allClear() {
-    const $total = document.querySelector('#total');
-    $total.innerText = '0';
+    const $total = document.querySelector("#total");
+    $total.innerText = "0";
+    this.operands = [];
+    this.operator = "";
   }
 }
 
