84fdca0fd46e19c5265502cbb5b92b1a8ed55441
SunnyK <sunnyk5780@gmail.com>
2021-02-03 17:22:05 +0900

    feat: 숫자 입력

---
+++ cypress/integration/examples.spec.js
@@ -0,0 +1,20 @@
+describe('click event', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5500/');
+  });
+
+  it('초기값이 0인지 확인', () => {
+    cy.get('#total').should('have.text', '0');
+  });
+
+  it('한 자리 숫자 입력', () => {
+    cy.get('.digit').contains('1').click();
+    cy.get('#total').should('have.text', '1');
+  });
+
+  it('두 자리 숫자 입력', () => {
+    cy.get('.digit').contains('1').click();
+    cy.get('.digit').contains('2').click();
+    cy.get('#total').should('have.text', '12');
+  });
+});
