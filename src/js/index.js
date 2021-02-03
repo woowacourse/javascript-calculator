a8870684257800373a616381252f8c37bf820cdc
iborymagic <mlch8023@gmail.com>
2021-02-03 18:20:10 +0900

    feat: operations & limitation - plus, minus, division, multiplication - 3 digit limitation - all clear

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,8 @@ const digits = document.getElementsByClassName('digit');
 const operators = document.getElementsByClassName('operator');
 const display = document.getElementById('total');
 const equal = document.getElementById('equal');
+const clear = document.querySelector('.modifier');
+let numLenChecker = 0;
 
 for (let i = 0; i < digits.length; i++) {
   digits[i].addEventListener('click', () => printDigit(digits[i].innerHTML));
@@ -13,9 +15,15 @@ for (let i = 0; i < operators.length; i++) {
   );
 }
 
-equal.addEventListener('click', () => printResult());
+equal.addEventListener('click', () => calculate());
+clear.addEventListener('click', printAC);
 
 function printDigit(digit) {
+  if (numLenChecker === 3) {
+    alert('숫자는 3자리까지만 입력이 가능합니다.');
+    return;
+  }
+  numLenChecker++;
   if (display.innerHTML === '0') {
     display.innerHTML = digit;
   } else {
@@ -24,19 +32,40 @@ function printDigit(digit) {
 }
 
 function printOperator(operator) {
+  numLenChecker = 0;
   display.innerHTML += operator;
 }
 
-function printResult() {
+function calculate() {
   const currInput = display.innerHTML;
-  if (currInput.includes('+')) {
-    const digit1 = parseInt(currInput.slice(0, currInput.indexOf('+')));
-    const digit2 = parseInt(currInput.slice(currInput.indexOf('+') + 1));
-    printAdd(digit1, digit2);
+  const pattern = /[+-/X]/g;
+  const operator = currInput.match(pattern)[0];
+  const digits = currInput.split(operator);
+  const digit1 = parseInt(digits[0]);
+  const digit2 = parseInt(digits[1]);
+
+  if (operator) {
+    printResult(digit1, digit2, operator);
   }
 }
 
-function printAdd(digit1, digit2) {
-  const result = digit1 + digit2;
+function printResult(digit1, digit2, operator) {
+  let result;
+  if (operator === '+') {
+    result = digit1 + digit2;
+  } else if (operator === '-') {
+    result = digit1 - digit2;
+  } else if (operator === '/') {
+    result = Math.floor(digit1 / digit2);
+  } else if (operator === 'X') {
+    result = digit1 * digit2;
+  }
+
+  display.innerHTML = result;
+}
+
+function printAC() {
+  const result = '0';
+  numLenChecker = 0;
   display.innerHTML = result;
 }
