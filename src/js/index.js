6ce8d77a84475a2a6c7f0e18ebda3601505de19f
365kim <365listener@gmail.com>
2021-02-04 15:13:31 +0900

    feat: 연산자 클릭 시 디스플레이에 표시하는 기능

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 const $total = document.querySelector('#total');
 const $digits = document.querySelector('.digits');
+const $operations = document.querySelector('.operations');
 
 const putNumber = ({ target }) => {
   if ($total.innerText === '0') {
@@ -7,5 +8,9 @@ const putNumber = ({ target }) => {
   }
   return ($total.innerText += target.innerText);
 };
+const putOperator = ({ target }) => {
+  return ($total.innerText += target.innerText);
+};
 
 $digits.addEventListener('click', putNumber);
+$operations.addEventListener('click', putOperator);
