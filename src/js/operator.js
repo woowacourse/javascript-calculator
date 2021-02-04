ceae5d3da0528e46ca688e2b7952623a9e781358
ChayanRin <k852012@naver.com>
2021-02-04 16:52:03 +0900

    feat : add, minus, multiply, divide, dropDecimalPoint 함수 구현

--- src/js/operator.js
+++ src/js/operator.js
@@ -1,7 +1,7 @@
-const add = (num1, num2) => {};
-const minus = (num1, num2) => {};
-const multiply = (num1, num2) => {};
-const divide = (num1, num2) => {};
-const dropDecimalPoint = num => {};
+const add = (num1, num2) => num1 + num2;
+const minus = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
+const dropDecimalPoint = num => parseInt(num, 10);
 
 export { add, minus, multiply, divide, dropDecimalPoint };
