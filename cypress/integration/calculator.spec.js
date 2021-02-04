8af28a9ef2daaf87a0bf3ff1392ff57640f3ec84
Kyle <mkitigy@gmail.com>
2021-02-04 16:10:37 +0900

    feat: add test functions
    - render initial value (0)
    - render digit when clicking digit button
    - check only continuous input (0)
    - check max digit length (3)

---
+++ cypress/integration/calculator.spec.js
@@ -0,0 +1,35 @@
+describe('Calculator test', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:5501/index.html');
+  });
+
+  it('Render initial value (0)', () => {
+    cy.get('#total').should('have.text', '0');
+  });
+
+  it('Render digit when clicking digit button', () => {
+    cy.get(`[data-test-digit='1']`).click();
+    cy.get('#total').should('have.text', '1');
+  });
+
+  it('Check only continuous input (0)', () => {
+    for (let i = 0; i < 3; i++) {
+      cy.get(`[data-test-digit='0']`).click();
+    }
+    cy.get('#total').should('have.text', '0');
+  });
+
+  it('Check max digit length (3)', () => {
+    for (let i = 1; i < 5; i++) {
+      cy.get(`[data-test-digit=${i}]`).click();
+    }
+    cy.get('#total').should('have.text', '123');
+  });
+
+  // 연산 테스트
+  // 소수점 계산을 했을 때 => 정수값으로 나오는지
+  // 연산 값이 음수일 때 => '-' 나오는지
+  // 초기화 버튼 눌렀을 때 => total 0
+  // 0으로 나누었을 때 => total Infinity
+  // 맨 처음에 연산 기호가 나왔을 때
+});
