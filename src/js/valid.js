f3ea49e7618057b1c5f08e7e1efd82cdcc8d1f2c
ChayanRin <k852012@naver.com>
2021-02-04 17:09:12 +0900

    feat : isNumberOverThreeChar 함수 구현

--- src/js/valid.js
+++ src/js/valid.js
@@ -1,4 +1,11 @@
-const isNumberOverThreeChar = operator => {};
+const isNumberOverThreeChar = operator => {
+  const splitedOperator = operator.split(/[*+-/]/);
+  if (splitedOperator.slice(-1)[0].length > 3) {
+    return alert("숫자는 3자리를 넘을 수 없습니다.");
+  }
+
+  return true;
+};
 const isOperatorOverTwoChar = operator => {};
 
 export { isNumberOverThreeChar, isOperatorOverTwoChar };
