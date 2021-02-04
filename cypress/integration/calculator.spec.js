376e7cd6f9334f411d94be27100ac87504c744ba
365kim <365listener@gmail.com>
2021-02-04 14:07:52 +0900

    test: 연산자 클릭 시 결과디스플레이 표시 테스트코드 작성, 예제코드 삭제

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -5,11 +5,22 @@ describe('calculator', () => {
 
   it('숫자버튼을 눌렀을 경우 결과디스플레이에 제대로 표시되는지 테스트 한다.', () => {
     cy.get('#total').then((display) => {
-      const prevInputText = display.text();
-      console.log(`prevInputText: ${prevInputText}`);
+      const displayText = display.text();
+      //console.log(`displayText: ${displayText}`);
 
       cy.get('.digits').contains('1').click();
       cy.get('#total').should('have.text', '1');
+    });
+  });
+
+  it('숫자버튼 입력 후 연산자를 클릭했을 경우 결과디스플레이에 제대로 표시되는지 테스트 한다.', () => {
+    cy.get('#total').then((display) => {
+      const displayText = display.text();
+      console.log(`displayText: ${displayText}`);
+
+      cy.get('.digits').contains('1').click();
+      cy.get('.operations').contains('/').click();
+      cy.get('#total').should('have.text', displayText + '/');
     });
   });
 });
