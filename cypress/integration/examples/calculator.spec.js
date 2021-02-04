a20283b67dee013bdcf68852005cdc33c823335c
ChayanRin <k852012@naver.com>
2021-02-04 11:24:41 +0900

    feat : 숫자 클릭시 입력되는 테스트코드 구현 완료

--- cypress/integration/examples/calculator.spec.js
+++ cypress/integration/examples/calculator.spec.js
@@ -1,10 +1,17 @@
-describe('ui-click', () => {
+describe("ui-click", () => {
   beforeEach(() => {
     // 페이지 접속. 띄워진 서버 port를 작성해주세요.
-    cy.visit('http://localhost:5500/');
+    cy.visit("http://localhost:5500/");
   });
 
   // 숫자 UI 클릭하면 입력된다
+  it("숫자를 클릭하면 입력된다", () => {
+    cy.get(".digit").each((num, i, array) => {
+      num.click();
+      cy.get("#total").should("have.text", num[0].outerText);
+      //cy.log("number", num[0].outerText);
+    });
+  });
 
   // 연산 기호를 클릭하면 입력된다 - 하드코딩
 
