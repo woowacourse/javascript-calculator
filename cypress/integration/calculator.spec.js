7be1737e6fb120565cc1bc4d8469911399535ce7
365kim <365listener@gmail.com>
2021-02-04 15:12:42 +0900

    test: 숫자버튼 입력 후 연산자 클릭 테스트코드 기댓값 수정

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -14,13 +14,10 @@ describe('calculator', () => {
   });
 
   it('숫자버튼 입력 후 연산자를 클릭했을 경우 결과디스플레이에 제대로 표시되는지 테스트 한다.', () => {
-    cy.get('#total').then((display) => {
-      const displayText = display.text();
-      console.log(`displayText: ${displayText}`);
-
+    cy.get('#total').then(() => {
       cy.get('.digits').contains('1').click();
       cy.get('.operations').contains('/').click();
-      cy.get('#total').should('have.text', displayText + '/');
+      cy.get('#total').should('have.text', '1/');
     });
   });
 
