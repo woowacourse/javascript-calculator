6cc74cdcd3c82e02050ea1828896b40c6bb46306
Shim MunSeong <Puterism.K@gmail.com>
2021-02-04 16:32:59 +0900

    refactor: 이벤트 함수를 메소드로 분리

--- src/js/index.js
+++ src/js/index.js
@@ -1,75 +1,77 @@
 class Calculator {
   constructor() {
-    // this.elements = {
-    //   total: document.querySelector('#total'),
-    //   digits: document.querySelector('.digits'),
-    //   modifiers: document.querySelector('.modifiers'),
-    //   operations: document.querySelector('.operations'),
-    // };
+    this.$ = {
+      total: document.querySelector('#total'),
+      digits: document.querySelector('.digits'),
+      modifiers: document.querySelector('.modifiers'),
+      operations: document.querySelector('.operations'),
+    };
+
     this.operands = [];
-    this.operator = "";
+    this.operator = '';
     this.isTimeToInputNumber = false;
     this.setEventListener();
   }
 
+  onClickDigit(event) {
+    if (event.target.className !== 'digit') return;
+
+    const digit = event.target.innerText;
+
+    if (this.$.total.innerText.length >= 3 && !this.isTimeToInputNumber) return;
+
+    if (this.$.total.innerText === '0' || this.isTimeToInputNumber) {
+      this.$.total.innerText = '';
+      this.isTimeToInputNumber = false;
+    }
+
+    this.$.total.innerText += digit;
+  }
+
+  onClickModifier(event) {
+    if (event.target.className !== 'modifier') return;
+
+    const modifier = event.target.innerText;
+
+    if (modifier === 'AC') {
+      this.allClear();
+    }
+  }
+
+  onClickOperation(event) {
+    if (event.target.className !== 'operation') return;
+
+    const operation = event.target.innerText;
+    this.operands.push(Number(this.$.total.innerText));
+
+    if (operation !== '=') {
+      this.operator = operation;
+      this.isTimeToInputNumber = true;
+      return;
+    }
+
+    let result = 0;
+    if (this.operator === '+') {
+      result = this.operands[0] + this.operands[1];
+    }
+    console.log(this.operands[0] + this.operands[1]);
+
+    this.$.total.innerText = result;
+  }
+
   setEventListener() {
-    const $digits = document.querySelector(".digits");
-    const $modifiers = document.querySelector(".modifiers");
-    const $operations = document.querySelector(".operations");
-    const $total = document.querySelector("#total");
-
-    $digits.addEventListener("click", (event) => {
-      if (event.target.className !== "digit") return;
-
-      const digit = event.target.innerText;
-
-      if ($total.innerText.length >= 3 && !this.isTimeToInputNumber) return;
-
-      if ($total.innerText === "0" || this.isTimeToInputNumber) {
-        $total.innerText = "";
-        this.isTimeToInputNumber = false;
-      }
-
-      $total.innerText += digit;
-    });
-
-    $modifiers.addEventListener("click", (event) => {
-      if (event.target.className !== "modifier") return;
-
-      const modifier = event.target.innerText;
-
-      if (modifier === "AC") {
-        this.allClear();
-      }
-    });
-
-    $operations.addEventListener("click", (event) => {
-      if (event.target.className !== "operation") return;
-
-      const operation = event.target.innerText;
-      this.operands.push(Number($total.innerText));
-
-      if (operation !== "=") {
-        this.operator = operation;
-        this.isTimeToInputNumber = true;
-        return;
-      }
-
-      let result = 0;
-      if (this.operator === "+") {
-        result = this.operands[0] + this.operands[1];
-      }
-      console.log(this.operands[0] + this.operands[1]);
-
-      $total.innerText = result;
-    });
+    this.$.digits.addEventListener('click', this.onClickDigit.bind(this));
+    this.$.modifiers.addEventListener('click', this.onClickModifier.bind(this));
+    this.$.operations.addEventListener(
+      'click',
+      this.onClickOperation.bind(this)
+    );
   }
 
   allClear() {
-    const $total = document.querySelector("#total");
-    $total.innerText = "0";
+    this.$.total.innerText = '0';
     this.operands = [];
-    this.operator = "";
+    this.operator = '';
   }
 }
 
