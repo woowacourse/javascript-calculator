56a4288af4d35e7b25f941da870d90269b4d01bf
ChayanRin <k852012@naver.com>
2021-02-04 11:55:09 +0900

    fix: 숫자 UI 클릭 테스트 반복 수행되게 수정

--- cypress/integration/examples/calculator.spec.js
+++ cypress/integration/examples/calculator.spec.js
@@ -5,12 +5,12 @@ describe("ui-click", () => {
   });
 
   // 숫자 UI 클릭하면 입력된다
-  /*   it("숫자를 클릭하면 입력된다", () => {
-    cy.get(".digit").each((num, i, array) => {
-      num.click();
+  it("숫자를 클릭하면 입력된다", () => {
+    cy.get(".digit").each(num => {
+      cy.get(num).click();
       cy.get("#total").should("have.text", num[0].outerText);
     });
-  }); */
+  });
 
   // 연산 기호를 클릭하면 입력된다 - 하드코딩
   it("연산 기호를 클릭하면 입력된다", () => {
@@ -23,6 +23,7 @@ describe("ui-click", () => {
     cy.get(".operation").eq(3).click().should("have.text", "+");
     cy.get("#total").should("have.text", "+");
   });
+
   // = 기호 클릭하면 계산되고 소수점 이하는 버린다
 
   // AC 누르면 초기화된다
