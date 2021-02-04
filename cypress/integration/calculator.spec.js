9a275f005f4a9e9ddb7a82b7e45e2e6009d02e95
yujo <yujo@student.42seoul.kr>
2021-02-04 14:49:58 +0900

    test: 사칙연산 결과값이 올바르게 나오는지 확인하는 테스트 코드 작성

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -54,31 +54,106 @@ describe('calculator', () => {
   //   });
   // });
 
-  it('현재 display의 마지막 텍스트가 숫자가 아닌 상태에서 연산자가 입력됐을 경우 경고메세지가 뜬다.', () => {
-    const stub = cy.stub();
+  // it('현재 display의 마지막 텍스트가 숫자가 아닌 상태에서 연산자가 입력됐을 경우 경고메세지가 뜬다.', () => {
+  //   const stub = cy.stub();
 
-    cy.on('window:alert', stub);
+  //   cy.on('window:alert', stub);
+  //   cy.get('#total').then(() => {
+  //     cy.get('.operations')
+  //       .contains('/')
+  //       .click()
+  //       .then(() => {
+  //         expect(stub.getCall(0)).to.be.calledWith(
+  //           '숫자를 먼저 입력한 후 연산자를 입력해주세요!',
+  //         );
+  //       });
+
+  //     cy.on('window:alert', stub);
+  //     cy.get('.digits').contains('1').click();
+  //     cy.get('.operations').contains('/').click();
+  //     cy.get('.operations')
+  //       .contains('/')
+  //       .click()
+  //       .then(() => {
+  //         expect(stub.getCall(0)).to.be.calledWith(
+  //           '숫자는 세 자리까지만 입력 가능합니다!',
+  //         );
+  //       });
+  //   });
+  // });
+
+  it('덧셈 연산 999 + 999의 결과값이 1998이 나오는지 확인한다.', () => {
     cy.get('#total').then(() => {
-      cy.get('.operations')
-        .contains('/')
-        .click()
-        .then(() => {
-          expect(stub.getCall(0)).to.be.calledWith(
-            '숫자를 먼저 입력한 후 연산자를 입력해주세요!',
-          );
-        });
+      cy.get('.digits').contains('9').click();
+      cy.get('.digits').contains('9').click();
+      cy.get('.digits').contains('9').click();
 
-      cy.on('window:alert', stub);
+      cy.get('.operations').contains('+').click();
+
+      cy.get('.digits').contains('9').click();
+      cy.get('.digits').contains('9').click();
+      cy.get('.digits').contains('9').click();
+
+      cy.get('#total').should('have.text', '1998');
+    });
+  });
+
+  it('뺄셈 연산 999 - 1의 결과값이 998이 나오는지 확인한다.', () => {
+    cy.get('#total').then(() => {
+      cy.get('.digits').contains('9').click();
+      cy.get('.digits').contains('9').click();
+      cy.get('.digits').contains('9').click();
+
+      cy.get('.operations').contains('-').click();
+
       cy.get('.digits').contains('1').click();
+
+      cy.get('#total').should('have.text', '998');
+    });
+  });
+
+  it('곱셈 연산 -999 * 999 의 결과값이 -998001이 나오는지 확인한다.', () => {
+    cy.get('#total').then(() => {
+      cy.get('.operations').contains('-').click();
+      cy.get('.digits').contains('9').click();
+      cy.get('.digits').contains('9').click();
+      cy.get('.digits').contains('9').click();
+
+      cy.get('.operations').contains('*').click();
+
+      cy.get('.digits').contains('9').click();
+      cy.get('.digits').contains('9').click();
+      cy.get('.digits').contains('9').click();
+
+      cy.get('#total').should('have.text', '-998001');
+    });
+  });
+
+  it('나눗셈 연산 999 / 9 의 결과값이 111이 나오는지 확인한다.', () => {
+    cy.get('#total').then(() => {
+      cy.get('.digits').contains('9').click();
+      cy.get('.digits').contains('9').click();
+      cy.get('.digits').contains('9').click();
+
       cy.get('.operations').contains('/').click();
-      cy.get('.operations')
-        .contains('/')
-        .click()
-        .then(() => {
-          expect(stub.getCall(0)).to.be.calledWith(
-            '숫자는 세 자리까지만 입력 가능합니다!',
-          );
-        });
+
+      cy.get('.digits').contains('9').click();
+
+      cy.get('#total').should('have.text', '111');
+    });
+  });
+
+  it('나눗셈 연산 999 / 5 의 결과값이 199이 나오는지 확인한다.', () => {
+    cy.get('#total').then(() => {
+      cy.get('.digits').contains('9').click();
+      cy.get('.digits').contains('9').click();
+      cy.get('.digits').contains('9').click();
+
+      cy.get('.operations').contains('/').click();
+
+      cy.get('.digits').contains('5').click();
+
+      cy.get('#total').should('have.text', '199');
     });
   });
 });
