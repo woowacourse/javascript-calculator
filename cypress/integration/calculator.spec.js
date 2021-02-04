820e0e34224ff92fbea20f0a110aed17a1bf7562
yujo <yujo@student.42seoul.kr>
2021-02-04 14:53:07 +0900

    test: AC버튼 클릭시 결과디스플레이값이 0으로 초기화 되는지 확인하는 테스트코드 작성

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -3,84 +3,88 @@ describe('calculator', () => {
     cy.visit('http://localhost:5500/');
   });
 
-  // it('숫자버튼을 눌렀을 경우 결과디스플레이에 제대로 표시되는지 테스트 한다.', () => {
-  //   cy.get('#total').then((display) => {
-  //     const displayText = display.text();
-  //     //console.log(`displayText: ${displayText}`);
+  it('숫자버튼을 눌렀을 경우 결과디스플레이에 제대로 표시되는지 테스트 한다.', () => {
+    cy.get('#total').then((display) => {
+      const displayText = display.text();
+      //console.log(`displayText: ${displayText}`);
 
-  //     cy.get('.digits').contains('1').click();
-  //     cy.get('#total').should('have.text', '1');
-  //   });
-  // });
+      cy.get('.digits').contains('1').click();
+      cy.get('#total').should('have.text', '1');
+    });
+  });
 
-  // it('숫자버튼 입력 후 연산자를 클릭했을 경우 결과디스플레이에 제대로 표시되는지 테스트 한다.', () => {
-  //   cy.get('#total').then((display) => {
-  //     const displayText = display.text();
-  //     console.log(`displayText: ${displayText}`);
+  it('숫자버튼 입력 후 연산자를 클릭했을 경우 결과디스플레이에 제대로 표시되는지 테스트 한다.', () => {
+    cy.get('#total').then((display) => {
+      const displayText = display.text();
+      console.log(`displayText: ${displayText}`);
 
-  //     cy.get('.digits').contains('1').click();
-  //     cy.get('.operations').contains('/').click();
-  //     cy.get('#total').should('have.text', displayText + '/');
-  //   });
-  // });
+      cy.get('.digits').contains('1').click();
+      cy.get('.operations').contains('/').click();
+      cy.get('#total').should('have.text', displayText + '/');
+    });
+  });
 
-  // it('네자리 이상의 숫자가 입력됐을 경우 경고메세지가 뜬다.', () => {
-  //   const stub = cy.stub();
+  it('네자리 이상의 숫자가 입력됐을 경우 경고메세지가 뜬다.', () => {
+    const stub = cy.stub();
 
-  //   cy.on('window:alert', stub);
+    cy.on('window:alert', stub);
 
-  //   cy.get('#total').then(() => {
-  //     for (let i = 0; i < 3; i++) {
-  //       cy.get('.digits').contains('1').click();
-  //     }
-  //     cy.get('.digits')
-  //       .contains('1')
-  //       .click()
-  //       .then(() => {
-  //         expect(stub.getCall(0)).to.be.calledWith('숫자는 세 자리까지만 입력 가능합니다!');
-  //       });
-  //     cy.get('.operations').contains('/').click();
-  //     for (let i = 0; i < 3; i++) {
-  //       cy.get('.digits').contains('1').click();
-  //     }
-  //     cy.get('.digits')
-  //       .contains('1')
-  //       .click()
-  //       .then(() => {
-  //         expect(stub.getCall(0)).to.be.calledWith('숫자는 세 자리까지만 입력 가능합니다!');
-  //       });
+    cy.get('#total').then(() => {
+      for (let i = 0; i < 3; i++) {
+        cy.get('.digits').contains('1').click();
+      }
+      cy.get('.digits')
+        .contains('1')
+        .click()
+        .then(() => {
+          expect(stub.getCall(0)).to.be.calledWith(
+            '숫자는 세 자리까지만 입력 가능합니다!',
+          );
+        });
+      cy.get('.operations').contains('/').click();
+      for (let i = 0; i < 3; i++) {
+        cy.get('.digits').contains('1').click();
+      }
+      cy.get('.digits')
+        .contains('1')
+        .click()
+        .then(() => {
+          expect(stub.getCall(0)).to.be.calledWith(
+            '숫자는 세 자리까지만 입력 가능합니다!',
+          );
+        });
 
-  //     cy.get('#total').should('have.text', '111/111');
-  //   });
-  // });
+      cy.get('#total').should('have.text', '111/111');
+    });
+  });
 
-  // it('현재 display의 마지막 텍스트가 숫자가 아닌 상태에서 연산자가 입력됐을 경우 경고메세지가 뜬다.', () => {
-  //   const stub = cy.stub();
+  it('현재 display의 마지막 텍스트가 숫자가 아닌 상태에서 연산자가 입력됐을 경우 경고메세지가 뜬다.', () => {
+    const stub = cy.stub();
 
-  //   cy.on('window:alert', stub);
-  //   cy.get('#total').then(() => {
-  //     cy.get('.operations')
-  //       .contains('/')
-  //       .click()
-  //       .then(() => {
-  //         expect(stub.getCall(0)).to.be.calledWith(
-  //           '숫자를 먼저 입력한 후 연산자를 입력해주세요!',
-  //         );
-  //       });
+    cy.on('window:alert', stub);
+    cy.get('#total').then(() => {
+      cy.get('.operations')
+        .contains('/')
+        .click()
+        .then(() => {
+          expect(stub.getCall(0)).to.be.calledWith(
+            '숫자를 먼저 입력한 후 연산자를 입력해주세요!',
+          );
+        });
 
-  //     cy.on('window:alert', stub);
-  //     cy.get('.digits').contains('1').click();
-  //     cy.get('.operations').contains('/').click();
-  //     cy.get('.operations')
-  //       .contains('/')
-  //       .click()
-  //       .then(() => {
-  //         expect(stub.getCall(0)).to.be.calledWith(
-  //           '숫자는 세 자리까지만 입력 가능합니다!',
-  //         );
-  //       });
-  //   });
-  // });
+      cy.on('window:alert', stub);
+      cy.get('.digits').contains('1').click();
+      cy.get('.operations').contains('/').click();
+      cy.get('.operations')
+        .contains('/')
+        .click()
+        .then(() => {
+          expect(stub.getCall(0)).to.be.calledWith(
+            '숫자는 세 자리까지만 입력 가능합니다!',
+          );
+        });
+    });
+  });
 
   it('덧셈 연산 999 + 999의 결과값이 1998이 나오는지 확인한다.', () => {
     cy.get('#total').then(() => {
@@ -154,6 +158,19 @@ describe('calculator', () => {
       cy.get('.digits').contains('5').click();
 
       cy.get('#total').should('have.text', '199');
+    });
+  });
+
+  it('AC버튼 클릭시 결과디스플레이의 값이 0으로 초기화되는지 확인한다.', () => {
+    cy.get('#total').then(() => {
+      cy.get('.digits').contains('9').click();
+      cy.get('.digits').contains('9').click();
+      cy.get('.digits').contains('9').click();
+      cy.get('.operations').contains('/').click();
+      cy.get('.digits').contains('5').click();
+      cy.get('.modifier').click();
+
+      cy.get('#total').should('have.text', '0');
     });
   });
 });
