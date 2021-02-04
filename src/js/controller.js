001a27bf0b5d1b1c358b6f45af68daf885cbaa10
ChayanRin <k852012@naver.com>
2021-02-04 17:34:43 +0900

    feat: operator 함수 구현

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,12 +1,29 @@
+import { add, minus, multiply, divide, dropDecimalPoint } from "./operator.js";
+
 class Calculator {
   constructor() {
     this.operator = "0";
   }
+
   addInput(value) {
-    // operator 변수에 value 추가
+    this.operator += value;
   }
 
-  operate(operation) {}
+  operate(operation) {
+    const inputOperator = operation.replace(/[0-9]/g, "");
+    const [num1, num2] = operation.split(/[*+-/]/).map(x => parseInt(x));
+    if ("+" === inputOperator) {
+      this.operator = add(num1, num2);
+    } else if ("-" === inputOperator) {
+      this.operator = minus(num1, num2);
+    } else if ("*" === inputOperator) {
+      this.operator = multiply(num1, num2);
+    } else if ("/" === inputOperator) {
+      this.operator = dropDecimalPoint(divide(num1, num2));
+    }
+
+    return this.operator;
+  }
 
   reset() {}
 }
