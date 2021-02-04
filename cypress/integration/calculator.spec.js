6476ca379d3366d3b57a93d42031334914959d69
Shim MunSeong <Puterism.K@gmail.com>
2021-02-04 16:06:33 +0900

    test: 2개의 임의의 3자리 숫자를 입력받고 빼는 테스트 코드 작성

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -27,4 +27,29 @@ context('calculator', () => {
 
     cy.get('#total').should('have.text', `${result}`);
   });
+
+  it('2개의 임의의 3자리 숫자를 입력받고 뺀다', () => {
+    const digits = [];
+
+    for (let i = 0; i < 2; i++) {
+      const randomDigit = `${Math.floor(Math.random() * 1000)}`;
+      digits.push(randomDigit);
+    }
+
+    const result = Number(digits[0]) - Number(digits[1]);
+
+    for (let i = 0; i < digits[0].length; i++) {
+      cy.get('.digit').contains(digits[0][i]).click();
+    }
+
+    cy.get('.operation').contains('-').click();
+
+    for (let i = 0; i < digits[1].length; i++) {
+      cy.get('.digit').contains(digits[1][i]).click();
+    }
+
+    cy.get('.operation').contains('=').click();
+
+    cy.get('#total').should('have.text', `${result}`);
+  });
 });
