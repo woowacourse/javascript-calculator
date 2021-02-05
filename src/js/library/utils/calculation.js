1912679708d3385251e80255e50e21f137eb858f
jho2301 <jho2301@gmail.com>
2021-02-05 22:51:17 +0900

    feat: 계산 결과를 표현할 때 소수점 이하는 버림한다.

--- src/js/library/utils/calculation.js
+++ src/js/library/utils/calculation.js
@@ -2,7 +2,7 @@ const operation = {
   ['-']: (a, b) => a - b,
   ['+']: (a, b) => a + b,
   ['X']: (a, b) => a * b,
-  ['/']: (a, b) => a / b,
+  ['/']: (a, b) => parseInt(a / b),
 };
 
 export default operation;
