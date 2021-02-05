3ab8b4be633dcb51d67af80f505baf7962ba360a
jum0 <wnsah052@naver.com>
2021-02-05 15:42:49 +0900

    refactor : function separation for reuse

--- src/js/index.js
+++ src/js/index.js
@@ -1,96 +1,73 @@
-let firstNumber = '0';
-let operator = '';
-let secondNumber = '0';
+const acElement = document.querySelector(".modifier");
+const operatorElements = document.querySelectorAll('.operation');
+const digitElements = document.querySelectorAll('.digit')
+const resultAreaElement = document.getElementById('total');
+
+const firstNumber = { value: '0', isEntered: false };
+const operator = { value: '+', isEntered: false };
+const secondNumber = { value: '0', isEntered: false };
 let result = '0';
 
+const clear = () => {
+  firstNumber.isEntered = secondNumber.isEntered = operator.isEntered = false;
+  firstNumber.value = secondNumber.value = '0';
+  operator.value = '+';
+  result = '0';
+  displayResult('0');
+}
+
+const isValidLength = (num) => {
+  if (num.charAt(0) === '-') {
+    if (num.length - 1 < 3) return true;
+  } else {
+    if (num.length < 3) return true;
+  }
+  return false;
+}
+
+const displayResult = (num) => resultAreaElement.innerText = num;
+
+const calculateResult = () => {
+  if (operator === '+')
+    return parseInt(firstNumber) + parseInt(secondNumber);
+  if (operator === '-')
+    return parseInt(firstNumber) - parseInt(secondNumber);
+  if (operator === 'X')
+    return parseInt(firstNumber) * parseInt(secondNumber);
+  if (operator === '/')
+    return parseInt(firstNumber) / parseInt(secondNumber);
+}
+
+// 숫자 버튼 클릭 시
+const digitCallback = (e) => {
+  // operator가 입력되지 않은 상태 => firstNumber 입력
+  // firstNumber가 입력되지 않은 상태 && 0~9숫자입력 또는 -이면 firstNumber에 입력
+
+  // firstNumber가 입력된 상태 && 0~9 숫자 입력 && 길이가 3미만이면 firstNumber에 이어 붙이기
+  // firstNumber가 0이면 덮어쓰기
+
+  // operator가 입력된 상태 => secondNumber 입력
+  // secondNumber가 입력되지 않았으면 덮어쓰기
+  // secondNumber가 0이면 덮어쓰기
+  // secondNumber가 입력되어 있고 길이가 3미만이면 이어 붙이기
+}
+
+// 연산자 버튼 클릭 시
+const operatorCallback = (e) => {
+  // 
+}
+
 const init = () => {
-  const AC = document.querySelector(".modifier");
-  const operatorElements = document.querySelectorAll('.operation');
-  const digits = document.querySelectorAll('.digit')
-  const resultArea = document.getElementById('total');
+  acElement.addEventListener("click", clear);
 
-  const clear = () => {
-    resultArea.innerText = '0';
-    firstNumber = '0';
-    operator = '';
-    secondNumber = '0';
-    result = '0';
-  }
+  digitElements.forEach(digit => {
+    digit.addEventListener('click', digitCallback);
+  });
 
-  const isValidLength = (num) => {
-    if (num.charAt(0) === '-') {
-      if (num.length - 1 < 3) return true;
-    } else {
-      if (num.length < 3) return true;
-    }
-    return false;
-  }
-
-  AC.addEventListener("click", clear);
-
-  digits.forEach(digit => {
-    digit.addEventListener('click', (e) => {
-      // console.log(e.target.innerText);
-      // firstNumber
-      if (!operator) {
-        if (firstNumber.charAt(0) === '0') {
-          firstNumber = e.target.innerText;
-        } else {
-          if (isValidLength(firstNumber)) {
-            firstNumber += e.target.innerText;
-          }
-        }
-        console.log(firstNumber);
-        resultArea.innerText = firstNumber;
-      } else { // secondNumber
-        if (isValidLength(secondNumber)) {
-          if (secondNumber === '0') {
-            secondNumber = e.target.innerText;
-          } else {
-            secondNumber += e.target.innerText;
-            resultArea.innerText = secondNumber;
-          }
-          console.log(secondNumber);
-        }
-      }
-
-    })
-  })
-
-  const calculateResult = () => {
-    if (operator === '+') {
-      result = parseInt(firstNumber) + parseInt(secondNumber);
-    } else if (operator === '-') {
-      result = parseInt(firstNumber) - parseInt(secondNumber);
-    } else if (operator === 'X') {
-      result = parseInt(firstNumber) * parseInt(secondNumber);
-    } else if (operator === '/') {
-      result = parseInt(firstNumber) / parseInt(secondNumber);
-    } else if (operator === '') {
-      result = parseInt(firstNumber);
-    }
-
-    console.log(result);
-    resultArea.innerText = `${result}`;
-  }
-
-  operatorElements.forEach((operatorElement, index) => {
-    if (index === 4) {
-      operatorElement.addEventListener('click', calculateResult);
-    } else {
-      operatorElement.addEventListener('click', (e) => {
-        if ((firstNumber === '-' || firstNumber === '0') && e.target.innerText === '-') {
-          firstNumber = '-';
-        } else {
-          if (firstNumber !== '0') {
-            operator = e.target.innerText;
-            console.log(operator);
-          }
-        }
-      });
-    }
-  })
+  operatorElements.forEach((operatorElement) => {
+    operatorElement.addEventListener('click', operatorCallback);
+  });
 };
 
-init();
 
+init();
