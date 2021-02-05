f1b86e0289a9ec9898df2e17d433dc361656588f
shinsehantan <shinsehantan@users.noreply.github.com>
2021-02-05 17:22:47 +0900

    add test case: 결과창(#total)의 기본 값이 "0"이어야 한다.

--- cypress/integration/e2e/e2e.spec.js
+++ cypress/integration/e2e/e2e.spec.js
@@ -1,114 +1,11 @@
 /// <reference types="cypress" />
 
-context("Actions", () => {
+context("e2e", () => {
   beforeEach(() => {
     cy.visit("http://127.0.0.1:5500/index.html");
   });
 
-  it("숫자 입력1", () => {
-    cy.get(".digits>.digit").eq(0).click();
-    cy.get(".digits>.digit").eq(1).click();
-    cy.get(".digits>.digit").eq(2).click();
-
-    cy.get(".operations>.operation").eq(0).click();
-
-    cy.get(".digits>.digit").eq(2).click();
-
-    cy.get("#total").should("have.value", "141");
-  });
-
-  it("숫자 입력2", () => {
-    cy.get(".digits>.digit").eq(3).click();
-    cy.get(".digits>.digit").eq(4).click();
-    cy.get(".digits>.digit").eq(5).click();
-
-    cy.get(".operations>.operation").eq(1).click();
-
-    cy.get(".digits>.digit").eq(0).click();
-    cy.get(".digits>.digit").eq(1).click();
-
-    cy.get("#total").should("have.value", "64092");
-  });
-
-  it("숫자 입력3", () => {
-    cy.get(".digits>.digit").eq(6).click();
-    cy.get(".digits>.digit").eq(7).click();
-    cy.get(".digits>.digit").eq(8).click();
-
-    cy.get(".operations>.operation").eq(2).click();
-
-    cy.get(".digits>.digit").eq(7).click();
-    cy.get(".digits>.digit").eq(8).click();
-
-    cy.get("#total").should("have.value", "300");
-  });
-
-  it("숫자 입력4", () => {
-    cy.get(".digits>.digit").eq(9).click();
-    cy.get(".digits>.digit").eq(9).click();
-    cy.get(".digits>.digit").eq(7).click();
-
-    cy.get(".operations>.operation").eq(3).click();
-
-    cy.get(".digits>.digit").eq(3).click();
-    cy.get(".digits>.digit").eq(6).click();
-    cy.get(".digits>.digit").eq(9).click();
-
-    cy.get(".operations>.operation").eq(4).click();
-    cy.get("#total").should("have.value", "632");
-  });
-
-  it("숫자 클릭 후 All Clear", () => {
-    cy.get(".digits>.digit").eq(3).click();
-    cy.get(".modifier").click();
-
-    cy.get("#total").should("have.value", "0");
-  });
-
-  it("숫자, 연산자 클릭 후 All Clear", () => {
-    cy.get(".digits>.digit").eq(3).click();
-    cy.get(".operations>.operation").eq(0).click();
-    cy.get(".modifier").click();
-
-    cy.get("#total").should("have.value", "0");
-  });
-
-  it("3개가 최대", () => {
-    cy.get(".digits>.digit").eq(0).click();
-    cy.get(".digits>.digit").eq(1).click();
-    cy.get(".digits>.digit").eq(2).click();
-    cy.get(".digits>.digit").eq(6).click();
-
-    cy.get("#total").should("have.value", "987");
-    cy.get(".digits>.digit").eq(0).click();
-    cy.get(".digits>.digit").eq(1).click();
-    cy.get(".digits>.digit").eq(2).click();
-    cy.get(".digits>.digit").eq(6).click();
-
-    cy.get("#total").should("have.value", "987");
-  });
-
-  it("소수점 버리기", () => {
-    cy.get(".digits>.digit").eq(8).click();
-    cy.get(".digits>.digit").eq(9).click();
-
-    cy.get(".operations>.operation").eq(0).click();
-
-    cy.get(".digits>.digit").eq(6).click();
-
-    cy.get(".operations>.operation").eq(4).click();
-    cy.get("#total").should("have.value", "3");
-  });
-
-  it("연산자 연속해서 누르면 마지막 연산자가 적용되야 한다.", () => {
-    cy.get(".digits>.digit").eq(3).click();
-
-    cy.get(".operations>.operation").eq(0).click();
-    cy.get(".operations>.operation").eq(3).click();
-
-    cy.get(".digits>.digit").eq(5).click();
-
-    cy.get(".operations>.operation").eq(4).click();
-    cy.get("#total").should("have.value", "10");
+  it(`결과창(#total)의 기본 값이 "0"이어야 한다.`, () => {
+    cy.get("#total").should("have.text", "0");
   });
 });
