e981a461b27a12584a9fa0e91312705cde06112a
jum0 <wnsah052@naver.com>
2021-02-05 17:14:14 +0900

    refactor : function separation for reuse

--- cypress/integration/examples/calculationTest.spec.js
+++ cypress/integration/examples/calculationTest.spec.js
@@ -17,25 +17,22 @@ const calculationTest = (num1, operator, num2, expectedResult) => {
   cy.get('.operation').contains('=').click();
 
   // index.html의 total 접근해서 total의 기댓값이 텍스트 56
-  cy.get('#total').should(($total) => {
-    expect($total).to.contain(`${expectedResult}`);
-    // expect($total).to.contain('59'); // wrong
-  });
+  cy.get('#total').should('have.text', `${expectedResult}`);
 
   cy.get('.modifier').click();
 }
 
 describe('사칙연산 테스트', () => {
-  // it('2개의 숫자에 대해 덧셈이 가능하다.', () => {
-  //   cy.visit('http://localhost:5500/index.html');
-  //   calculationTest('0', '+', '0', '0');
-  //   calculationTest('1', '+', '0', '1');
-  //   calculationTest('999', '+', '999', '1998');
-  //   calculationTest('055', '+', '45', '100');
-  //   calculationTest('5000', '+', '87', '587');
-  //   calculationTest('-11', '+', '10', '-1');
-  //   calculationTest('X8', '+', '12', '20');
-  // });
+  it('2개의 숫자에 대해 덧셈이 가능하다.', () => {
+    cy.visit('http://localhost:5500/index.html');
+    calculationTest('0', '+', '0', '0');
+    calculationTest('1', '+', '0', '1');
+    calculationTest('999', '+', '999', '1998');
+    calculationTest('055', '+', '45', '100');
+    calculationTest('5000', '+', '87', '587');
+    calculationTest('-11', '+', '10', '-1');
+    calculationTest('X8', '+', '12', '20');
+  });
 
   it('2개의 숫자에 대해 뺄셈이 가능하다.', () => {
     cy.visit('http://localhost:5500/index.html');
@@ -47,21 +44,21 @@ describe('사칙연산 테스트', () => {
     calculationTest('-0', '-', '1', '-1');
     calculationTest('-15', '-', '85', '-100');
     calculationTest('----2', '-', '2', '-4');
-    calculationTest('2', '+', '2-', '0');
-    // calculationTest('2', '-', '2-2-2-2-2', '-8');
+    calculationTest('2', '+', '2-', '4');
+    calculationTest('2', '-', '2-2-2-2-2', '-8');
     // 2+2- => 0
     // 2-2-2- => -2
   });
 
-  // it('2개의 숫자에 대해 곱셈이 가능하다.', () => {
-  //   cy.visit('http://localhost:5500/index.html');
-  //   calculationTest('7', 'X', '8', '56');
-  //   calculationTest('00', 'X', '0', '0');
-  //   calculationTest('999', 'X', '999', '998001');
-  //   calculationTest('-9999', 'X', '999', '998001');
-  //   calculationTest('-1', 'X', '09', '-9');
-  //   calculationTest('-0-0-0-0-0--1', 'X', '09', '-9');
-  //   calculationTest('00009', 'X', '-09', '-81');
+  it('2개의 숫자에 대해 곱셈이 가능하다.', () => {
+    cy.visit('http://localhost:5500/index.html');
+    calculationTest('7', 'X', '8', '56');
+    calculationTest('00', 'X', '0', '0');
+    calculationTest('999', 'X', '999', '998001');
+    calculationTest('-9999', 'X', '999', '-998001');
+    calculationTest('-1', 'X', '09', '-9');
+    calculationTest('-0-0-0-0-0--1', 'X', '09', '-9');
+    calculationTest('00009', 'X', '-09', '0');
 
-  // });
+  });
 })
--- src/js/index.js
+++ src/js/index.js
@@ -6,13 +6,11 @@ const resultAreaElement = document.getElementById('total');
 const firstNumber = { value: '0', isEntered: false };
 const operator = { value: '+', isEntered: false };
 const secondNumber = { value: '0', isEntered: false };
-let result = '0';
 
 const clear = () => {
   firstNumber.isEntered = secondNumber.isEntered = operator.isEntered = false;
   firstNumber.value = secondNumber.value = '0';
   operator.value = '+';
-  result = '0';
   displayResult('0');
 }
 
@@ -28,44 +26,117 @@ const isValidLength = (num) => {
 const displayResult = (num) => resultAreaElement.innerText = num;
 
 const calculateResult = () => {
-  if (operator === '+')
-    return parseInt(firstNumber) + parseInt(secondNumber);
-  if (operator === '-')
-    return parseInt(firstNumber) - parseInt(secondNumber);
-  if (operator === 'X')
-    return parseInt(firstNumber) * parseInt(secondNumber);
-  if (operator === '/')
-    return parseInt(firstNumber) / parseInt(secondNumber);
+  if (operator.value === '+')
+    return parseInt(firstNumber.value) + parseInt(secondNumber.value);
+  if (operator.value === '-')
+    return parseInt(firstNumber.value) - parseInt(secondNumber.value);
+  if (operator.value === 'X')
+    return parseInt(firstNumber.value) * parseInt(secondNumber.value);
+  if (operator.value === '/')
+    return parseInt(firstNumber.value) / parseInt(secondNumber.value);
+}
+
+function handleFirstNumber(chr) {
+  // 첫 번쨰 입력
+  // firstNumber가 입력되지 않은 상태 && (0~9숫자입력 또는 -이면) firstNumber에 입력
+  if (!firstNumber.isEntered && (('0' <= chr && chr <= '9') || chr === '-')) {
+    firstNumber.value = chr;
+    firstNumber.isEntered = true;
+    displayResult(firstNumber.value);
+    console.log(firstNumber.value, operator.value, secondNumber.value);
+    return;
+  }
+
+  // 두 번째 입력
+  // firstNumber가 입력된 상태 && 0~9 숫자 입력 && 길이가 3미만이면 firstNumber에 이어 붙이기
+  if (firstNumber.isEntered && ('0' <= chr && chr <= '9') && isValidLength(firstNumber.value)) {
+    // firstNumber가 0이거나 (firstNumber가 -이고 chr가 0인경우 덮어쓰기)
+    if (firstNumber.value === '0' || (firstNumber.value === '-' && chr === '0')) {
+      firstNumber.value = chr;
+      displayResult(firstNumber.value);
+      console.log(firstNumber.value, operator.value, secondNumber.value);
+      return;
+    }
+
+    firstNumber.value += chr;
+    displayResult(firstNumber.value);
+    console.log(firstNumber.value, operator.value, secondNumber.value);
+  }
+}
+
+function handleSecondNumber(chr) {
+  // secondNumber가 입력되지 않았으면 덮어쓰기
+  if (!secondNumber.isEntered) {
+    secondNumber.value = chr;
+    secondNumber.isEntered = true;
+    displayResult(secondNumber.value);
+    console.log(firstNumber.value, operator.value, secondNumber.value);
+    return;
+  }
+  // secondNumber가 0이면 덮어쓰기
+  if (secondNumber.value === '0') {
+    secondNumber.value = chr;
+    displayResult(secondNumber.value);
+    console.log(firstNumber.value, operator.value, secondNumber.value);
+    return;
+  }
+  // secondNumber가 입력되어 있고 길이가 3미만이면 이어 붙이기
+  if (isValidLength(secondNumber.value)) {
+    secondNumber.value += chr;
+    displayResult(secondNumber.value);
+    console.log(firstNumber.value, operator.value, secondNumber.value);
+  }
 }
 
 // 숫자 버튼 클릭 시
-const digitCallback = (e) => {
+function handleDigitElement(e) {
+  const chr = e.target.innerText;
   // operator가 입력되지 않은 상태 => firstNumber 입력
-  // firstNumber가 입력되지 않은 상태 && 0~9숫자입력 또는 -이면 firstNumber에 입력
-
-  // firstNumber가 입력된 상태 && 0~9 숫자 입력 && 길이가 3미만이면 firstNumber에 이어 붙이기
-  // firstNumber가 0이면 덮어쓰기
-
+  if (!operator.isEntered) {
+    handleFirstNumber(chr);
+    return;
+  }
   // operator가 입력된 상태 => secondNumber 입력
-  // secondNumber가 입력되지 않았으면 덮어쓰기
-  // secondNumber가 0이면 덮어쓰기
-  // secondNumber가 입력되어 있고 길이가 3미만이면 이어 붙이기
+  handleSecondNumber(chr);
 }
 
 // 연산자 버튼 클릭 시
-const operatorCallback = (e) => {
-  // 
+const handleOperatorElement = (e) => {
+  const chr = e.target.innerText;
+
+  // firstNumber가 입력안되어 있으면 firstNumber입력
+  if (!firstNumber.isEntered) {
+    handleFirstNumber(chr);
+    return;
+  }
+
+  // secondNumber가 입력되어 있으면 = 역할
+  if (secondNumber.isEntered) {
+    const result = calculateResult();
+    displayResult(result);
+    firstNumber.value = result;
+    operator.value = chr;
+    secondNumber.value = '0';
+    secondNumber.isEntered = false;
+    return;
+  }
+
+  if (chr !== '=' && firstNumber.value !== '-') {
+    operator.value = chr;
+    operator.isEntered = true;
+    console.log(firstNumber.value, operator.value, secondNumber.value);
+  }
 }
 
 const init = () => {
   acElement.addEventListener("click", clear);
 
   digitElements.forEach(digit => {
-    digit.addEventListener('click', digitCallback);
+    digit.addEventListener('click', handleDigitElement);
   });
 
   operatorElements.forEach((operatorElement) => {
-    operatorElement.addEventListener('click', operatorCallback);
+    operatorElement.addEventListener('click', handleOperatorElement);
   });
 };
 
