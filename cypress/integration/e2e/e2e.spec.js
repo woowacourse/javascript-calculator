7d9b516063de6aee2e6f0457a552ef681fcdaa03
dawit jeong <wjdekdnlt1215@gmail.com>
2021-02-05 17:30:35 +0900

    add test case: 결과창(#total)이 0일 때 숫자(.digit)을 클릭하면 그 숫자가 결과값이 되야 한다.

--- cypress/integration/e2e/e2e.spec.js
+++ cypress/integration/e2e/e2e.spec.js
@@ -8,4 +8,9 @@ context("e2e", () => {
   it(`결과창(#total)의 기본 값이 "0"이어야 한다.`, () => {
     cy.get("#total").should("have.text", "0");
   });
+
+  it(`결과창(#total)이 "0"일 때 숫자(.digit)을 클릭하면 그 숫자가 결과값이 되야 한다.`, () => {
+    cy.get(".digit").contains("1").click();
+    cy.get("#total").should("have.text", "1");
+  });
 });
