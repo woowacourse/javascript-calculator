d9583036d6fc152e6d7b59f263efcb1cad6ad268
sunhpark42 <sunhpark42@gmail.com>
2021-02-05 17:25:00 +0900

    Test : ignore over 3 digits

--- cypress/integration/calculatorTest.spec.js
+++ cypress/integration/calculatorTest.spec.js
@@ -1,14 +1,27 @@
 describe('calculator-test', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:5500/Lv1/javascript-calculator');
+    cy.visit('http://localhost:5500/');
   });
 
-  it('숫자를 눌렀을 때 화면에 표시되는지 확인한다.', () => {
-    const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    // 9 ~ 0
-    numbers.forEach((number) => {
-      cy.get('.digit').contains(number).click();
-      cy.get('#total').should('have.text', number);
-    });
+  it('숫자를 눌렀을 때 화면에 표시되고, 입력된 수는 누적된다.', () => {
+    cy.get('.digit').contains('1').click();
+    cy.get('#total').should('have.text', '1');
+    cy.get('.digit').contains('2').click();
+    cy.get('#total').should('have.text', '12');
+    cy.get('.digit').contains('3').click();
+    cy.get('#total').should('have.text', '123');
+  });
+
+  it('숫자가 3개이상 입력된 후에는 숫자가 더이상 입력되지 않는다.', () => {
+    cy.get('.digit').contains('1').click();
+    cy.get('#total').should('have.text', '1');
+    cy.get('.digit').contains('2').click();
+    cy.get('#total').should('have.text', '12');
+    cy.get('.digit').contains('3').click();
+    cy.get('#total').should('have.text', '123');
+    cy.get('.digit').contains('4').click();
+    cy.get('#total').should('have.text', '123');
+    cy.get('.digit').contains('5').click();
+    cy.get('#total').should('have.text', '123');
   });
 });
