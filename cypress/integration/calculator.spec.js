7004756a7af81b61fb8b4485c95b9a1f0119c7e9
swon3210 <swon3210@gmail.com>
2021-02-05 17:03:36 +0900

    refactor: 함수를 활용한 테스트 코드 리팩터링

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -1,55 +1,50 @@
 import { OPERAND_LENGTH_EXCEEDED_LIMIT } from '../../src/contants.js';
 
+function testCalculation(leftOperand, operator, rightOperand, expectedResult) {
+  cy.get('.digits').contains(leftOperand).click();
+  cy.get('#total').should('contain', leftOperand);
+  cy.get('.operations').contains(operator).click();
+  cy.get('#total').should('contain', operator);
+  cy.get('.digits').contains(rightOperand).click();
+  cy.get('#total').should('contain', rightOperand);
+  cy.get('.operations').contains('=').click();
+  cy.get('#total').should('contain', expectedResult);
+}
+
 describe('calculate', () => {
   beforeEach(() => {
     cy.visit('http://127.0.0.1:5501/');
   });
 
   it('2개의 숫자에 대해 덧셈', () => {
-    cy.get('.digit:nth-child(9)').click();
-    cy.get('.operation:nth-child(4)').click();
-    cy.get('.digit:nth-child(8)').click();
-    cy.get('.operation:nth-child(5)').click();
-    cy.get('#total').should('have.text', '3');
+    testCalculation('1', '+', '2', '3');
   });
 
   it('2개의 숫자에 대해 뺄셈', () => {
-    cy.get('.digit:nth-child(8)').click();
-    cy.get('.operation:nth-child(3)').click();
-    cy.get('.digit:nth-child(9)').click();
-    cy.get('.operation:nth-child(5)').click();
-    cy.get('#total').should('have.text', '1');
+    testCalculation('3', '-', '2', '1');
   });
 
   it('2개의 숫자에 대해 곱셈', () => {
-    cy.get('.digit:nth-child(8)').click();
-    cy.get('.operation:nth-child(2)').click();
-    cy.get('.digit:nth-child(8)').click();
-    cy.get('.operation:nth-child(5)').click();
-    cy.get('#total').should('have.text', '4');
+    testCalculation('3', 'X', '2', '6');
   });
 
   it('2개의 숫자에 대해 나눗셈', () => {
-    cy.get('.digit:nth-child(8)').click();
-    cy.get('.operation:nth-child(1)').click();
-    cy.get('.digit:nth-child(8)').click();
-    cy.get('.operation:nth-child(5)').click();
-    cy.get('#total').should('have.text', '1');
+    testCalculation('4', '/', '2', '2');
   });
 
   it('AC(All Clear)버튼을 누르면 0으로 초기화', () => {
-    cy.get('.digit:nth-child(8)').click();
-    cy.get('.digit:nth-child(8)').click();
+    cy.get('.digits').contains('2').click();
+    cy.get('.digits').contains('2').click();
     cy.get('#total').should('have.text', '22');
     cy.get('.modifier').click();
     cy.get('#total').should('have.text', '0');
   });
 
   it('숫자 자리수 제한 테스트', () => {
-    cy.get('.digit:nth-child(8)').click();
-    cy.get('.digit:nth-child(8)').click();
-    cy.get('.digit:nth-child(8)').click();
-    cy.get('.digit:nth-child(8)').click();
+    cy.get('.digits').contains('2').click();
+    cy.get('.digits').contains('2').click();
+    cy.get('.digits').contains('2').click();
+    cy.get('.digits').contains('2').click();
     cy.on('window:alert', (txt) => {
       expect(txt).to.contains(OPERAND_LENGTH_EXCEEDED_LIMIT);
     });
@@ -57,11 +52,6 @@ describe('calculate', () => {
   });
 
   it('계산 결과를 표현할 때 소수점 이하는 버림', () => {
-    cy.get('.digit:nth-child(8)').click();
-    cy.get('.digit:nth-child(8)').click();
-    cy.get('.operation:nth-child(1)').click();
-    cy.get('.digit:nth-child(7)').click();
-    cy.get('.operation:nth-child(5)').click();
-    cy.get('#total').should('have.text', '7');
+    testCalculation('7', '/', '2', '3');
   });
 });
