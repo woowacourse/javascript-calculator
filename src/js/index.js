854c94547edb464a9df462c0296cb00c89390293
jum0 <wnsah052@naver.com>
2021-02-05 17:58:06 +0900

    refactor : Decrease functions under 15 lines

--- src/js/index.js
+++ src/js/index.js
@@ -11,7 +11,7 @@ const clear = () => {
   firstNumber.isEntered = secondNumber.isEntered = operator.isEntered = false;
   firstNumber.value = secondNumber.value = '0';
   operator.value = '+';
-  displayResult('0');
+  displayResult(firstNumber.value);
 }
 
 const isValidLength = (num) => {
@@ -37,108 +37,78 @@ const calculateResult = () => {
 }
 
 function handleFirstNumber(chr) {
-  // 첫 번쨰 입력
-  // firstNumber가 입력되지 않은 상태 && (0~9숫자입력 또는 -이면) firstNumber에 입력
   if (!firstNumber.isEntered && (('0' <= chr && chr <= '9') || chr === '-')) {
     firstNumber.value = chr;
     firstNumber.isEntered = true;
     displayResult(firstNumber.value);
-    console.log(firstNumber.value, operator.value, secondNumber.value);
     return;
   }
-
-  // 두 번째 입력
-  // firstNumber가 입력된 상태 && 0~9 숫자 입력 && 길이가 3미만이면 firstNumber에 이어 붙이기
-  if (firstNumber.isEntered && ('0' <= chr && chr <= '9') && isValidLength(firstNumber.value)) {
-    // firstNumber가 0이거나 (firstNumber가 -이고 chr가 0인경우 덮어쓰기)
+  if (('0' <= chr && chr <= '9') && isValidLength(firstNumber.value)) {
     if (firstNumber.value === '0' || (firstNumber.value === '-' && chr === '0')) {
       firstNumber.value = chr;
       displayResult(firstNumber.value);
-      console.log(firstNumber.value, operator.value, secondNumber.value);
       return;
     }
-
     firstNumber.value += chr;
     displayResult(firstNumber.value);
-    console.log(firstNumber.value, operator.value, secondNumber.value);
   }
 }
 
 function handleSecondNumber(chr) {
-  // secondNumber가 입력되지 않았으면 덮어쓰기
-  if (!secondNumber.isEntered) {
+  if (!secondNumber.isEntered || secondNumber.value === '0') {
     secondNumber.value = chr;
     secondNumber.isEntered = true;
     displayResult(secondNumber.value);
-    console.log(firstNumber.value, operator.value, secondNumber.value);
     return;
   }
-  // secondNumber가 0이면 덮어쓰기
-  if (secondNumber.value === '0') {
-    secondNumber.value = chr;
-    displayResult(secondNumber.value);
-    console.log(firstNumber.value, operator.value, secondNumber.value);
-    return;
-  }
-  // secondNumber가 입력되어 있고 길이가 3미만이면 이어 붙이기
   if (isValidLength(secondNumber.value)) {
     secondNumber.value += chr;
     displayResult(secondNumber.value);
-    console.log(firstNumber.value, operator.value, secondNumber.value);
   }
 }
 
-// 숫자 버튼 클릭 시
-function handleDigitElement(e) {
+const handleDigitElement = (e) => {
   const chr = e.target.innerText;
-  // operator가 입력되지 않은 상태 => firstNumber 입력
   if (!operator.isEntered) {
     handleFirstNumber(chr);
     return;
   }
-  // operator가 입력된 상태 => secondNumber 입력
   handleSecondNumber(chr);
 }
 
-// 연산자 버튼 클릭 시
+const substituteSecondNumberToFirstNumber = (chr, result) => {
+  firstNumber.value = result;
+  operator.value = chr;
+  secondNumber.value = '0';
+  secondNumber.isEntered = false;
+}
+
 const handleOperatorElement = (e) => {
   const chr = e.target.innerText;
-
-  // firstNumber가 입력안되어 있으면 firstNumber입력
   if (!firstNumber.isEntered) {
     handleFirstNumber(chr);
     return;
   }
-
-  // secondNumber가 입력되어 있으면 = 역할
   if (secondNumber.isEntered) {
     const result = calculateResult();
     displayResult(result);
-    firstNumber.value = result;
-    operator.value = chr;
-    secondNumber.value = '0';
-    secondNumber.isEntered = false;
+    substituteSecondNumberToFirstNumber(chr, result);
     return;
   }
-
   if (chr !== '=' && firstNumber.value !== '-') {
     operator.value = chr;
     operator.isEntered = true;
-    console.log(firstNumber.value, operator.value, secondNumber.value);
   }
 }
 
 const init = () => {
   acElement.addEventListener("click", clear);
-
   digitElements.forEach(digit => {
     digit.addEventListener('click', handleDigitElement);
   });
-
   operatorElements.forEach((operatorElement) => {
     operatorElement.addEventListener('click', handleOperatorElement);
   });
 };
-
 
 init();
