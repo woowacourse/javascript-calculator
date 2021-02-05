1e49c04f08da563f928e1fc864295052cf63d1ef
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-02-05 16:59:21 +0900

    Fix : delete conflict test code

--- cypress/integration/calculatorTest.spec.js
+++ cypress/integration/calculatorTest.spec.js
@@ -3,16 +3,7 @@ describe('calculator-test', () => {
     cy.visit('http://localhost:5500/');
   });
 
-  it('숫자를 눌렀을 때 화면에 표시되는지 확인한다.', () => {
-    const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    // 9 ~ 0
-    numbers.forEach((number) => {
-      cy.get('.digit').contains(number).click();
-      cy.get('#total').should('have.text', number);
-    });
-  });
-
-  it('숫자를 여러번 눌렀을 때, 수가 누적된다.', () => {
+  it('숫자를 눌렀을 때 화면에 표시되고, 입력된 수는 누적된다.', () => {
     cy.get('.digit').contains('1').click();
     cy.get('#total').should('have.text', '1');
     cy.get('.digit').contains('2').click();
