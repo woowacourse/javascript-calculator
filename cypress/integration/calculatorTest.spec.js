218b6e5f974548d7b950d07c77cb343a9cbab343
sunhpark42 <sunhpark42@gmail.com>
2021-02-06 17:42:53 +0900

    Test : multiply 2 numbers

--- cypress/integration/calculatorTest.spec.js
+++ cypress/integration/calculatorTest.spec.js
@@ -66,4 +66,20 @@ describe('calculator-test', () => {
     cy.get('.operation').contains('=').click();
     cy.get('#total').should('have.text', '948');
   });
+
+  it('두 수의 곱셈이 가능하다', () => {
+    cy.get('.digit').contains('6').click();
+    cy.get('#total').should('have.text', '6');
+    cy.get('.digit').contains('5').click();
+    cy.get('#total').should('have.text', '65');
+    cy.get('.digit').contains('4').click();
+    cy.get('#total').should('have.text', '654');
+    cy.get('.operation').contains('X').click();
+    cy.get('.digit').contains('4').click();
+    cy.get('#total').should('have.text', '4');
+    cy.get('.digit').contains('6').click();
+    cy.get('#total').should('have.text', '46');
+    cy.get('.operation').contains('=').click();
+    cy.get('#total').should('have.text', '30084');
+  });
 });
