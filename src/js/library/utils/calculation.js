5c3c27be74c6c7ecdbc83a9dcc3991c449e5f366
jho2301 <jho2301@gmail.com>
2021-02-06 11:18:31 +0900

    feat: 0으로 나누면 Infinity를 출력한다.

--- src/js/library/utils/calculation.js
+++ src/js/library/utils/calculation.js
@@ -2,7 +2,7 @@ const operation = {
   ['-']: (a, b) => a - b,
   ['+']: (a, b) => a + b,
   ['X']: (a, b) => a * b,
-  ['/']: (a, b) => parseInt(a / b),
+  ['/']: (a, b) => Math.floor(a / b),
 };
 
 export default operation;
