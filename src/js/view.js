25b7d968e98b85bb9083b615ad6038dca14323e8
ChayanRin <k852012@naver.com>
2021-02-06 13:12:40 +0900

    refactor : handle 메소드들 이벤트 위임 적용

--- src/js/view.js
+++ src/js/view.js
@@ -7,24 +7,20 @@ class CalculatorView {
   }
 
   handleNumber() {
-    const digitBtns = document.querySelectorAll(".digit");
-    digitBtns.forEach(digitBtn => {
-      digitBtn.addEventListener("click", () => {
-        this.calculator.addNumber(digitBtn.innerHTML);
-      });
+    const digitBtns = document.querySelector(".digits");
+    digitBtns.addEventListener("click", e => {
+      this.calculator.addNumber(e.target.innerHTML);
     });
   }
 
   handleOperator() {
-    const operationBtns = document.querySelectorAll(".operation");
-    operationBtns.forEach(operationBtn => {
-      operationBtn.addEventListener("click", () => {
-        if (operationBtn.innerHTML === "=") {
-          return this.calculator.operate();
-        }
+    const operationBtns = document.querySelector(".operations");
+    operationBtns.addEventListener("click", e => {
+      if (e.target.innerHTML === "=") {
+        return this.calculator.operate();
+      }
 
-        return this.calculator.addOperator(operationBtn.innerHTML);
-      });
+      return this.calculator.addOperator(e.target.innerHTML);
     });
   }
 
