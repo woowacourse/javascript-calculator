f57d07d85da2018b62eed6d7931bc56210e1e1e0
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-02-09 23:29:37 +0900

    chore: 테스트코드 작성

---
+++ cypress/integration/calculator.spec.js
@@ -0,0 +1,81 @@
+// - [ ] 2개의 숫자에 대해 덧셈이 가능하다.
+it("2개의 숫자에 대해 덧셈이 가능하다.", () => {
+    cy.visit("index.html");
+
+    // 2+9 = 11
+    cy.get(".digit").contains("2").click();
+    cy.get(".operation").contains("+").click();
+    cy.get(".digit").contains("9").click();
+
+    cy.get(".operation").contains("=").click();
+    cy.get("#total").should("have.text", "11");
+});
+
+// - [ ] 2개의 숫자에 대해 뺄셈이 가능하다.
+it("2개의 숫자에 대해 뺄셈이 가능하다.", () => {
+    cy.visit("index.html");
+
+    cy.get(".digit").contains("2").click();
+    cy.get(".operation").contains("-").click();
+    cy.get(".digit").contains("9").click();
+
+    cy.get(".operation").contains("=").click();
+    cy.get("#total").should("have.text", "-7");
+});
+
+// - [ ] 2개의 숫자에 대해 곱셈이 가능하다.
+it("2개의 숫자에 대해 곱셈이 가능하다.", () => {
+    cy.visit("index.html");
+
+    cy.get(".digit").contains("2").click();
+    cy.get(".operation").contains("*").click();
+    cy.get(".digit").contains("9").click();
+
+    cy.get(".operation").contains("=").click();
+    cy.get("#total").should("have.text", "18");
+});
+
+// - [ ] 2개의 숫자에 대해 나눗셈이 가능하다.
+it("2개의 숫자에 대해 나눗셈이 가능하다.", () => {
+    cy.visit("index.html");
+
+    cy.get(".digit").contains("8").click();
+    cy.get(".operation").contains("/").click();
+    cy.get(".digit").contains("2").click();
+
+    cy.get(".operation").contains("=").click();
+    cy.get("#total").should("have.text", "4");
+});
+
+// - [ ] AC(All Clear)버튼을 누르면 0으로 초기화 한다.
+it("AC(All Clear)버튼을 누르면 0으로 초기화 한다.", () => {
+    cy.visit("index.html");
+
+    cy.get(".modifier").click();
+
+    cy.get("#total").should("have.text", "0");
+});
+
+// - [ ] 숫자는 한번에 최대 3자리 수까지 입력 가능하다.
+it("숫자는 한번에 최대 3자리 수까지 입력 가능하다.", () => {
+    cy.visit("index.html");
+
+    cy.get(".digit").contains("9").click();
+    cy.get(".digit").contains("9").click();
+    cy.get(".digit").contains("9").click();
+    cy.get(".digit").contains("9").click();
+
+    cy.get("#total").should("have.text", "999");
+});
+
+// - [ ] 계산 결과를 표현할 때 소수점 이하는 버림한다.
+it("계산 결과를 표현할 때 소수점 이하는 버림한다.", () => {
+    cy.visit("index.html");
+
+    cy.get(".digit").contains("5").click();
+    cy.get(".operation").contains("/").click();
+    cy.get(".digit").contains("3").click();
+    cy.get(".operation").contains("=").click();
+
+    cy.get("#total").should("have.text", "1");
+});
