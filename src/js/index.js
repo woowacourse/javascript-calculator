67c3483c02e1c92da64b6c785f099cf02b206104
gytks4 <gytks4@naver.com>
2022-02-09 22:45:06 +0900

    refactor: 메소드 분리

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ import { isUnderThreeDigits } from '../utils/validator.js';
 class Calculator {
   constructor() {
     this.configureButton();
-    this.$totalNumber = $('#total');
+    this.$total = $('#total');
     this.firstNumber = '0';
     this.secondNumber = '0';
     this.operator = '=';
@@ -20,61 +20,73 @@ class Calculator {
   }
 
   clickNumberButton = (e) => {
+    const clickedNumber = e.target.innerText;
     if (this.operator === '=') {
-      if (this.firstNumber === '0') {
-        this.firstNumber = e.target.innerText;
-        this.$totalNumber.innerText = e.target.innerText;
-        return;
-      }
-      if (!isUnderThreeDigits(this.firstNumber)) {
-        return;
-      }
-      this.firstNumber += e.target.innerText;
-      this.$totalNumber.innerText += e.target.innerText;
+      this.controlFirstNumber(clickedNumber);
       return;
     }
+    this.controlSecondNumber(clickedNumber);
+  };
+
+  controlFirstNumber = (clickedNumber) => {
+    if (this.firstNumber === '0') {
+      this.firstNumber = clickedNumber;
+      this.$total.innerText = clickedNumber;
+      return;
+    }
+    if (!isUnderThreeDigits(this.firstNumber)) {
+      return;
+    }
+    this.firstNumber += clickedNumber;
+    this.showTotal(clickedNumber);
+  };
+
+  controlSecondNumber = (clickedNumber) => {
     if (this.secondNumber === '0') {
-      this.secondNumber = e.target.innerText;
-      this.$totalNumber.innerText += e.target.innerText;
+      this.secondNumber = clickedNumber;
+      this.showTotal(clickedNumber);
       return;
     }
+
     if (!isUnderThreeDigits(this.secondNumber)) {
       return;
     }
-    this.secondNumber += e.target.innerText;
-    this.$totalNumber.innerText += e.target.innerText;
+    this.secondNumber += clickedNumber;
+    this.showTotal(clickedNumber);
+  };
+
+  showTotal = (number) => {
+    this.$total.innerText += number;
   };
 
   clickOperationButton = (e) => {
-    if (e.target.innerText === '=') {
-      // 결과를 계산
+    const clickedOperator = e.target.innerText;
+    if (clickedOperator === '=') {
       const result = this.calculate(
         Number(this.firstNumber),
         Number(this.secondNumber),
         this.operator,
       );
-      this.$totalNumber.innerText = result;
-      this.operator = e.target.innerText;
-      this.nextCalculate(result);
+      this.$total.innerText = result;
+      this.operator = clickedOperator;
+      this.resetForNextCalculate(result);
       return;
     }
-    this.operator = e.target.innerText;
-    this.$totalNumber.innerText += this.operator;
+    this.operator = clickedOperator;
+    this.showTotal(this.operator);
   };
 
-  clickModifierButton = (e) => {
-    this.$totalNumber.innerText = '0';
-    this.firstNumber = '0';
-    this.secondNumber = '0';
-    this.operator = '=';
-  };
-
-  nextCalculate(result) {
+  resetForNextCalculate(result) {
     this.firstNumber = result;
     this.secondNumber = '0';
   }
 
-  // 숫자 누를때마다 바로 결과값으로 보이게.
+  clickModifierButton = () => {
+    this.$total.innerText = '0';
+    this.firstNumber = '0';
+    this.secondNumber = '0';
+    this.operator = '=';
+  };
 
   calculate(firstNumber, secondNumber, operator) {
     switch (operator) {
