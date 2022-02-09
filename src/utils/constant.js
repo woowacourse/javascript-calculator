6fd53886cbe30619caea3a438529e85add730b10
gytks4 <gytks4@naver.com>
2022-02-09 22:22:23 +0900

    refactor: 상수 분리

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,5 @@
-export const EXAMPLE = 1;
+export const MAX_INPUT_LENGTH = 3;
+
+export const USER_INPUT_ALERT = {
+  over4DigitsError: '4자리 이상의 숫자를 입력할 수 없습니다.',
+};
--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,6 +1,8 @@
+import { USER_INPUT_ALERT, MAX_INPUT_LENGTH } from './constant.js';
+
 export const isUnderThreeDigits = (number) => {
-  if (number.length === 3) {
-    alert('4자리 이상의 숫자를 입력할 수 없습니다.');
+  if (number.length === MAX_INPUT_LENGTH) {
+    alert(USER_INPUT_ALERT.over4DigitsError);
     return false;
   }
   return true;
