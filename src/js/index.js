3d778e3591e740d81ab7a123308a87d95c8bd464
uk960214 <uk960214@gmail.com>
2022-02-09 18:05:13 +0900

    feat(eventHanlder): 숫자와 연산자를 클릭 시 계산기 클래스에 저장

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,107 @@
+import {
+  DOM,
+  ERROR_MESSAGE,
+  ONE_THOUSAND,
+  TYPE,
+  OPERATION,
+  ZERO,
+} from "./lib/constants.js";
+
 class Calculator {
-  constructor() {}
+  constructor() {
+    // 이벤트 핸들러를 붙일 돔 요소를 가져온다.
+    this.firstOperand = 0;
+    this.secondOperand;
+    this.currentOperator;
+    this.initDOM();
+    this.initHandler();
+  }
+  clearCalculator() {
+    this.firstOperand = 0;
+    this.secondOperand;
+    this.currentOperator;
+  }
+  initDOM() {
+    this.calculatorElement = document.querySelector(
+      `.${DOM.CALCULATOR_CLASS_NAME}`
+    );
+    this.totalElement = document.querySelector(`#${DOM.TOTAL_ID}`);
+  }
+
+  initHandler() {
+    // 핸들러 함수를 정의하고
+    // 여기서 핸들러를 바인딩
+    this.clickEventListener = (e) => {
+      const {
+        target: { className: targetClassName, textContent: targetTextContent },
+      } = e;
+      if (targetClassName === DOM.DIGIT_CLASS_NAME) {
+        this.addToOperand(targetTextContent);
+        return;
+      }
+      if (
+        targetClassName === DOM.OPERATION_CLASS_NAME &&
+        targetTextContent !== OPERATION.EQUAL
+      ) {
+        this.createOperator(targetTextContent);
+        return;
+      }
+
+      if (targetClassName === DOM.OPERATION_CLASS_NAME) {
+        // 결과 표시 함수
+        return;
+      }
+
+      if (targetClassName === DOM.MODIFIER_CLASS_NAME) {
+        return;
+      }
+    };
+    this.calculatorElement.addEventListener("click", this.clickEventListener);
+  }
+  addToOperand(numberStr) {
+    if (this.currentOperator) {
+      // 두번째 피연산자
+      this.secondOperand = this.secondOperand
+        ? Number(this.secondOperand + numberStr)
+        : Number(numberStr);
+    }
+    //첫번째 피연산자
+    this.firstOperand = Number(this.firstOperand + numberStr);
+  }
+  createOperator(operatorStr) {
+    this.currentOperator = operatorStr;
+  }
+  hasEmptyParameters(number1, number2) {
+    return number1 === TYPE.UNDEFINED || number2 === TYPE.UNDEFINED;
+  }
+
+  hasNonNumbers(number1, number2) {
+    return typeof number1 !== TYPE.NUMBER || typeof number2 !== TYPE.NUMBER;
+  }
+
+  hasDigitOver(number1, number2, standard) {
+    return number1 >= standard || number2 >= standard;
+  }
+
+  isNumberZero(number) {
+    return number === 0;
+  }
 
   checkParameter(number1, number2) {
-    if (number1 === undefined || number2 === undefined) {
-      throw Error("두 개의 인자를 입력해주세요.");
+    if (this.hasEmptyParameters(number1, number2)) {
+      throw Error(ERROR_MESSAGE.PARAMETER_ERROR);
     }
-    if (typeof number1 !== "number" || typeof number2 !== "number") {
-      throw Error("인자 타입 지정");
+    if (this.hasNonNumbers(number1, number2)) {
+      throw Error(ERROR_MESSAGE.TYPE_ERROR);
     }
-    if (number1 >= 1000 || number2 >= 1000) {
-      throw Error("3자리수 이하만 가능");
+    if (this.hasDigitOver(number1, number2, ONE_THOUSAND)) {
+      throw Error(ERROR_MESSAGE.NUMBER_SIZE_ERROR);
     }
   }
 
   checkDenominator(number) {
-    if (number === 0) {
-      throw Error("분모가 0일 수 없음");
+    if (this.isNumberZero(number)) {
+      throw Error(ERROR_MESSAGE.DENOMINATOR_ERROR);
     }
   }
 
@@ -42,17 +128,3 @@ class Calculator {
 }
 
 const calculator = new Calculator();
-
-// try {
-//   console.log(calculator.divide(100, 0));
-// } catch (error) {
-//   alert(error);
-// }
-// console.log(calculator.add(100, 100));
-// console.log(calculator.add(1000, 1000));
-// console.log(calculator.add("", 1000));
-// console.log(calculator.add());
-// console.log(calculator.add("string", "string"));
-// console.log(calculator.add(100, "string"));
-// console.log(calculator.divide(100, 10));
-// console.log(calculator.divide(100, 0));
---
+++ src/js/lib/constants.js
@@ -0,0 +1,31 @@
+export const DOM = Object.freeze({
+  CALCULATOR_CLASS_NAME: "calculator",
+  TOTAL_ID: "total",
+  DIGIT_CLASS_NAME: "digit",
+  OPERATION_CLASS_NAME: "operation",
+  MODIFIER_CLASS_NAME: "modifier",
+});
+
+export const TYPE = Object.freeze({
+  UNDEFINED: "undefined",
+  NUMBER: "number",
+});
+
+export const OPERATION = Object.freeze({
+  ADD: "+",
+  SUBTRACT: "-",
+  MULTIPLY: "*",
+  DIVIDE: "/",
+  EQUAL: "=",
+});
+
+export const ZERO = 0;
+
+export const ONE_THOUSAND = 1000;
+
+export const ERROR_MESSAGE = Object.freeze({
+  PARAMETER_ERROR: "두 개의 인자를 입력해주세요.",
+  TYPE_ERROR: "숫자만 입력이 가능합니다.",
+  NUMBER_SIZE_ERROR: "3자리수 이하만 입력 가능합니다.",
+  DENOMINATOR_ERROR: "분모는 0일 수 없습니다.",
+});
