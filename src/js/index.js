1ba98f8167714eafafe729400b196b3e12524278
woose28 <dldntmdsla@naver.com>
2022-02-09 21:12:15 +0900

    Feat: 이벤트 위임을 적용한 operations 이벤트 핸들러 등록

--- src/js/index.js
+++ src/js/index.js
@@ -53,6 +53,61 @@ class Calculator {
         return;
       }
     });
+
+    this.$operations.addEventListener('click', (e) => {
+      if (e.target.classList.contains('operation')) {
+        const [firstOperand, secondOperand] = this.operands;
+
+        if (e.target.innerText === '=') {
+          //추후 구현
+          //피연산자 연산자 멤버변수를 초기화한다.
+
+          //결과값을 렌더링한다.
+          switch (this.operator) {
+            case '+':
+              this.$total.innerText = Number(firstOperand) + Number(secondOperand);
+              break;
+            case '-':
+              this.$total.innerText = Number(firstOperand) - Number(secondOperand);
+              break;
+            case '/':
+              // 소수 점 없애는 작업
+              // 두번째 피연산자가 0인경우 예외처리
+              if (secondOperand === '0') {
+                alert('0으로 나눌 수 없습니다.');
+                return;
+              }
+              this.$total.innerText = Math.floor(Number(firstOperand) / Number(secondOperand));
+              break;
+            case 'X':
+              this.$total.innerText = Number(firstOperand) * Number(secondOperand);
+              break;
+            default:
+          }
+
+          return;
+        }
+
+        // operator 멤버 변수 체크
+        if (this.operator === null) {
+          this.operator = e.target.innerText;
+          this.$total.innerText = this.operator;
+          return;
+        }
+
+        // operator는 있는데, 첫번째 operand가 없을때
+        if (this.operands[0] === '') {
+          alert('연산자는 단독으로 사용할 수 없습니다');
+          return;
+        }
+
+        // 두번째 피연산자가 '' 빈문자열이 아닐때 에러처리
+        if (this.operands[1] !== '') {
+          alert('두 개의 피연산자만 연산이 가능합니다.');
+          return;
+        }
+      }
+    });
   }
 }
 
