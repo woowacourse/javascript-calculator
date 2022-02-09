aff204200576b63bf78825fa42c6dd768ab2fbab
jhy979 <jhy979@gmail.com>
2022-02-09 21:29:03 +0900

    feat: 숫자를 형식에 맞게 수정한다.

--- src/js/index.js
+++ src/js/index.js
@@ -5,11 +5,44 @@ class Calculator {
     this.number1 = 0;
     this.number2 = 0;
     this.operator = "";
+    this.flag = true;
     this.numberEvent();
     this.operatorEvent();
     this.resultEvent();
-    this.flag = true;
     this.reset();
+  }
+
+  numberEvent() {
+    // operator 없는 경우
+    $$(".digit").forEach((digit) => {
+      digit.addEventListener("click", (e) => {
+        if (this.operator === "") {
+          if (String($("#total").innerText).length >= 3) {
+            alert("3자리 숫자까지만 허용됩니다.");
+            return;
+          }
+
+          $("#total").innerText =
+            Number($("#total").innerText) * 10 + Number(e.target.textContent);
+          return;
+          // num1만 입력되는 과정
+        }
+        if (this.operator !== "" && this.flag) {
+          // + 누르고 난 직후의 작업
+          this.flag = false;
+          $("#total").innerText = e.target.textContent;
+          return;
+        }
+
+        if (String($("#total").innerText).length >= 3) {
+          alert("3자리 숫자까지만 허용됩니다.");
+          return;
+        }
+
+        $("#total").innerText =
+          Number($("#total").innerText) * 10 + Number(e.target.textContent);
+      });
+    });
   }
 
   operatorEvent() {
@@ -59,7 +92,11 @@ class Calculator {
       if (this.operator === "/") {
         if (this.number2 === 0) {
           alert("0으로 나눌 수 없습니다.");
-          this.reset();
+          this.number1 = 0;
+          this.number2 = 0;
+          this.operator = "";
+          $("#total").innerHTML = 0;
+          this.flag = true;
           return;
         }
         $("#total").innerHTML = Math.floor(this.number1 / this.number2);
