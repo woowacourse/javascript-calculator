69aa45d1ea3cb08226949bc09ef8f6838e32ec21
Hwang Jung Min <hwangstar156@gmail.com>
2022-02-09 17:15:06 +0900

    refactor: 세미콜론 누락된것 추가

--- src/js/index.js
+++ src/js/index.js
@@ -3,19 +3,19 @@ class Calculator {
     }
 
     init = () => {
-        this.bindEvent()
+        this.bindEvent();
     }
 
     bindEvent = () => {
-        document.querySelector(".digits").addEventListener("click", this.clickDigitHandler)
+        document.querySelector(".digits").addEventListener("click", this.clickDigitHandler);
     }
     
     clickDigitHandler = (e) => {
         const selectedNumber = e.target.innerText;
         const totalResult = document.getElementById("total");
-        totalResult.innerText = selectedNumber
+        totalResult.innerText = selectedNumber;
     }
 }
 
-const calculator = new Calculator()
-calculator.init()
+const calculator = new Calculator();
+calculator.init();
