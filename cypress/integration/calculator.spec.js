0e644ca734df47453d0d2d4b09075660969816a4
jhy979 <jhy979@gmail.com>
2022-02-09 21:51:28 +0900

    test: 2개의 숫자에 대해 곱셈이 가능하다.

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -16,3 +16,12 @@ it("2개의 숫자에 대해 뺄셈이 가능하다.", () => {
   cy.get(".operation").contains("=").click();
   cy.get("#total").should("have.text", "7");
 });
+
+it("2개의 숫자에 대해 곱셈이 가능하다.", () => {
+  cy.visit("index.html");
+  cy.get(".digit").contains(5).click();
+  cy.get(".operation").contains("X").click();
+  cy.get(".digit").contains(3).click();
+  cy.get(".operation").contains("=").click();
+  cy.get("#total").should("have.text", "15");
+});
