747a5a45086559849b6fd3d08c76f23365becefc
gytks4 <gytks4@naver.com>
2022-02-09 21:19:41 +0900

    feat: 사칙역산 구현

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,9 @@ import { $ } from '../utils/common.js';
 class Calculator {
   constructor() {
     this.configureButton();
-    this.firstNumber;
-    this.secondNumber;
+    this.$totalNumber = $('#total');
+    this.firstNumber = 0;
+    this.secondNumber = 0;
     this.operator = '=';
   }
 
@@ -17,11 +18,53 @@ class Calculator {
     $operationButtons.addEventListener('click', this.clickOperationButton);
   }
 
-  clickNumberButton = (e) => {};
+  clickNumberButton = (e) => {
+    console.log(e.target.innerText);
+    if (this.operator === '=') {
+      this.firstNumber += e.target.innerText;
+      this.$totalNumber.innerText = this.firstNumber;
+      return;
+    }
+    this.secondNumber = e.target.innerText;
+    this.$totalNumber.innerText += this.secondNumber;
+  };
 
-  clickModifierButton = (e) => {};
+  clickOperationButton = (e) => {
+    if (e.target.innerText === '=') {
+      // 결과를 계산
+      const result = this.calculate(
+        Number(this.firstNumber),
+        Number(this.secondNumber),
+        this.operator,
+      );
+      this.$totalNumber.innerText = result;
+      this.operator = e.target.innerText;
+      this.nextCalculate(result);
+      return;
+    }
+    this.operator = e.target.innerText;
+    this.$totalNumber.innerText += this.operator;
+  };
 
-  clickOperationButton = (e) => {};
+  // 숫자 누를때마다 바로 결과값으로 보이게.
+
+  calculate(firstNumber, secondNumber, operator) {
+    console.log('firstNumber', firstNumber);
+    console.log('secondNumber', secondNumber);
+    switch (operator) {
+      case '+':
+        return firstNumber + secondNumber;
+      case '-':
+        return firstNumber - secondNumber;
+      case 'X':
+        return firstNumber * secondNumber;
+      case '/':
+        return firstNumber / secondNumber;
+      default:
+        // 예외 처리
+        return 0;
+    }
+  }
 }
 
 const calculator = new Calculator();
