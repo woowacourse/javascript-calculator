885e7598ebacdcec904f6fb572cd2227bc40f238
kkojae <jaejeung3210@gmail.com>
2022-02-09 22:40:10 +0900

    feat: AC 버튼 클릭 테스트 케이스 작성

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -42,7 +42,14 @@ it("2개의 숫자에 대해 나눗셈이 가능하다.", () => {
   cy.get("#total").should("have.text", "2");
 });
 
-// AC(All Clear)버튼을 누르면 0으로 초기화 한다.
+it("AC(All Clear)버튼을 누르면 0으로 초기화 한다.", () => {
+  cy.visit("index.html");
+
+  cy.get(".digit").contains(4).click();
+  cy.get(".modifier").contains("AC").click();
+  cy.get("#total").should("have.text", "0");
+});
+
 // 숫자는 한번에 최대 3자리 수까지 입력 가능하다.
 // 숫자는 2개까지만 입력할 수 있다.
 // 계산 결과를 표현할 때 소수점 이하는 버림한다.
