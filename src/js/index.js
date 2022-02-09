96456b42885ba37b7d6b17b93d0b3978a0212e15
woose28 <dldntmdsla@naver.com>
2022-02-09 21:08:36 +0900

    Feat: 이벤트 위임을 적용한 digits 이벤트 핸들러 등록

--- src/js/index.js
+++ src/js/index.js
@@ -17,7 +17,43 @@ class Calculator {
     this.$operations = document.getElementsByClassName('operations')[0];
   }
 
-  initEventListner() {}
+  initEventListner() {
+    this.$digits.addEventListener('click', (e) => {
+      if (e.target.classList.contains('digit')) {
+        const number = e.target.innerText;
+        const [firstOperand, secondOperand] = this.operands;
+
+        if (this.operator === null) {
+          // 첫 번째 피연산자 입력
+          // 길이 체크하기
+          if (firstOperand.length === 3) {
+            alert('숫자는 세 자리를 초과하면 안됩니다.');
+            return;
+          }
+
+          // 숫자를 이어붙인다.
+          this.operands[0] += number;
+          this.$total.innerText = this.operands[0];
+
+          return;
+        }
+
+        // 두 번째 피연산자 입력
+        // 길이 체크하기
+        if (secondOperand.length === 3) {
+          alert('숫자는 세 자리를 초과하면 안됩니다.');
+          return;
+        }
+
+        // 숫자를 이어붙인다.
+        this.operands[1] += number;
+        this.$total.innerText = this.operands[1];
+        console.log(this.operator);
+        console.log(this.operands);
+        return;
+      }
+    });
+  }
 }
 
 new Calculator();
