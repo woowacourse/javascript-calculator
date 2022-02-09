66e3183d78a1ae24da8bdcb3b399e831d22b6421
kkojae <jaejeung3210@gmail.com>
2022-02-09 22:30:44 +0900

    feat: 뺄셈, 곱셈, 나눗셈 테스트 케이스 작성

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -9,9 +9,39 @@ it("2개의 숫자에 대해 덧셈이 가능하다.", () => {
   cy.get("#total").should("have.text", "11");
 });
 
-// 2개의 숫자에 대해 뺄셈이 가능하다.
-// 2개의 숫자에 대해 곱셈이 가능하다.
-// 2개의 숫자에 대해 나눗셈이 가능하다.
+it("2개의 숫자에 대해 뺄셈이 가능하다.", () => {
+  cy.visit("index.html");
+
+  // 7 - 2 = 5
+  cy.get(".digit").contains(7).click();
+  cy.get(".operation").contains("-").click();
+  cy.get(".digit").contains(2).click();
+  cy.get(".operation").contains("=").click();
+  cy.get("#total").should("have.text", "5");
+});
+
+it("2개의 숫자에 대해 곱셈이 가능하다.", () => {
+  cy.visit("index.html");
+
+  // 2 * 9 = 18
+  cy.get(".digit").contains(2).click();
+  cy.get(".operation").contains("X").click();
+  cy.get(".digit").contains(9).click();
+  cy.get(".operation").contains("=").click();
+  cy.get("#total").should("have.text", "18");
+});
+
+it("2개의 숫자에 대해 나눗셈이 가능하다.", () => {
+  cy.visit("index.html");
+
+  // 10 / 2 = 5
+  cy.get(".digit").contains(10).click();
+  cy.get(".operation").contains("-").click();
+  cy.get(".digit").contains(2).click();
+  cy.get(".operation").contains("=").click();
+  cy.get("#total").should("have.text", "5");
+});
+
 // AC(All Clear)버튼을 누르면 0으로 초기화 한다.
 // 숫자는 한번에 최대 3자리 수까지 입력 가능하다.
 // 숫자는 2개까지만 입력할 수 있다.
