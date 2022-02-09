0aa2af9bd2fd8992a3228bf080fd004319d05c16
kkojae <jaejeung3210@gmail.com>
2022-02-10 00:23:46 +0900

    feat: 숫자가 3개 이상 입력되면 alert 보여주는 기능 구현

--- src/js/model/Model.js
+++ src/js/model/Model.js
@@ -10,11 +10,13 @@ export default class Model {
   addUserInputString(character) {
     if (typeof character === "number") {
       this.digitCount += 1;
-      console.log(this.digitCount);
+
       if (this.digitCount > 3) {
         alert("alert");
+
         this.initializeDigitCount();
         this.initializeUserInputString();
+
         return "0";
       }
     }
@@ -30,6 +32,18 @@ export default class Model {
 
   calculate() {
     let result;
+
+    if (
+      this.userInputString.split(/[\+\-X/]+/).filter((elem) => typeof Number(elem) === "number")
+        .length > 2
+    ) {
+      alert("숫자는 두개까지 입력 가능합니다.");
+
+      this.initializeDigitCount();
+      this.initializeUserInputString();
+
+      return "0";
+    }
 
     if (this.userInputString.includes("+")) {
       const [number1, number2] = this.userInputString.split("+");
