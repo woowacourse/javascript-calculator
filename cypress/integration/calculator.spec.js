791d589134d2d25af03cc9cabd56a4ed65260342
uk960214 <uk960214@gmail.com>
2022-02-09 20:28:21 +0900

    test: AC, 버림, 검증 test 코드 작성

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -77,3 +77,44 @@ it("2개의 숫자에 대해 나눗셈이 가능하다", () => {
   // 4가 나오는지 확인한다.
   cy.get("#total").should("have.text", "4");
 });
+
+it("AC(All Clear)버튼을 누르면 0으로 초기화 한다.", () => {
+  cy.visit("index.html");
+
+  // AC를 클릭한다.
+  cy.get(".modifier").click();
+
+  // 0이 나오는지 확인한다.
+  cy.get("#total").should("have.text", "0");
+});
+
+it("숫자는 한번에 최대 3자리 수까지 입력 가능하다.", () => {
+  cy.visit("index.html");
+
+  const target = cy.get(".digit").contains(8);
+  const alertStub = cy.stub();
+
+  cy.on("window:alert", alertStub);
+
+  target.click();
+  target.click();
+  target.click();
+
+  target.click().then(() => {
+    expect(alertStub).to.be.called;
+  });
+});
+
+it("계산 결과를 표현할 때 소수점 이하는 버림한다.", () => {
+  cy.visit("index.html");
+
+  cy.get(".digit").contains(8).click();
+
+  cy.get(".operation").contains("/").click();
+
+  cy.get(".digit").contains(3).click();
+
+  cy.get(".operation").contains("=").click();
+
+  cy.get("#total").should("have.text", "2");
+});
