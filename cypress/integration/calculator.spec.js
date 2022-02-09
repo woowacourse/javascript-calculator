4a974af4b77ab3794969135d32ba50179bf8dcd6
sangminsong <ssmin940606@gmail.com>
2022-02-09 21:27:39 +0900

    test: 덧셈 기능에 대한 테스트 코드 추가

---
+++ cypress/integration/calculator.spec.js
@@ -0,0 +1,20 @@
+// - [ ] 2개의 숫자에 대해 뺄셈이 가능하다.
+// - [ ] 2개의 숫자에 대해 곱셈이 가능하다.
+// - [ ] 2개의 숫자에 대해 나눗셈이 가능하다.
+// - [ ] AC(All Clear)버튼을 누르면 0으로 초기화 한다.
+// - [ ] 숫자는 한번에 최대 3자리 수까지 입력 가능하다.
+// - [ ] 계산 결과를 표현할 때 소수점 이하는 버림한다.
+
+it('2개의 숫자에 대해 덧셈이 가능하다.', () => {
+  cy.visit('index.html');
+  // 6을 누른다.
+  cy.get('.digit').contains(6).click();
+  // +를 누른다.
+  cy.get('.operation').contains('+').click();
+  // 3을 누른다.
+  cy.get('.digit').contains(3).click();
+  // =을 누른다.
+  cy.get('.operation').contains('=').click();
+  // 9가 도출된다.
+  cy.get('#total').should('have.text', '9');
+});
