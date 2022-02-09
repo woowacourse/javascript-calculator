3f9662d4e83137744904198661549ca6a607cf5b
Hwang Jung Min <hwangstar156@gmail.com>
2022-02-09 17:21:00 +0900

    feat: 숫자가 3자리수가 넘어간 입력 들어갈 경우 alert로 예외 처리

--- src/js/index.js
+++ src/js/index.js
@@ -3,16 +3,19 @@ class Calculator {
         this.currentNumber = '';
     }
 
-
     init = () => {
-        this.bindEvent();
+        this.bindEvent()
     }
 
     bindEvent = () => {
-        document.querySelector(".digits").addEventListener("click", this.clickDigitHandler);
+        document.querySelector(".digits").addEventListener("click", this.clickDigitHandler)
     }
-    
+
     clickDigitHandler = (e) => {
+        if (this.isOverThreeDigit()) {
+            alert("3글자가 넘어갔어요..");
+            return;
+        }
         const selectedNumber = e.target.innerText;
         this.currentNumber += selectedNumber;
         this.renderResult();
@@ -21,6 +24,10 @@ class Calculator {
     renderResult = (e) => {
         const totalResult = document.getElementById("total");
         totalResult.innerText = this.currentNumber;
+    }
+
+    isOverThreeDigit = () => {
+        return this.currentNumber.length >= 3;
     }
 }
 
