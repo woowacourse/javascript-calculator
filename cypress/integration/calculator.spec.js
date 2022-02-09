a08cbbadd7241b2c84ba3109ec27f0078c5c1eb8
intae92 <kangit2013@gamil.com>
2022-02-09 21:47:43 +0900

    cypress 테스트 적용용

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -1,31 +1,66 @@
 // - [ ] 2개의 숫자에 대해 덧셈이 가능하다.
 
-it("그냥 적어봄", () => {
-
+it('2개의 숫자에 대해 덧셈이 가능하다', () => {
     cy.visit('index.html');
-    cy.get('#total').should('have.text','0');
-    
-    // // given
-    // const alertStub = cy.stub();
-    // const invalidInput = -1;
-
-    // cy.on("window:alert", alertStub);
-
-    // // when
-    // cy.get(SELECTOR.COIN_MENU).click();
-    // cy.get(SELECTOR.COIN_CHARGE_INPUT).type(invalidInput);
-
-    // // then
-    // cy.get(SELECTOR.COIN_CHARGE_BUTTON)
-    //   .click()
-    //   .then(() => {
-    //     expect(alertStub).to.be.called;
-    // });
+    cy.get(".digit").contains(2).click();
+    cy.get(".operation").contains("+").click();
+    cy.get(".digit").contains(9).click();
+    cy.get(".operation").contains("=").click();
+    cy.get('#total').should('have.text','11');
 });
 
 // - [ ] 2개의 숫자에 대해 뺄셈이 가능하다.
+
+it('2개의 숫자에 대해 뺄셈이 가능하다', () => {
+    cy.visit('index.html');
+    cy.get(".digit").contains(9).click();
+    cy.get(".operation").contains("-").click();
+    cy.get(".digit").contains(8).click();
+    cy.get(".operation").contains("=").click();
+    cy.get('#total').should('have.text','1');
+});
+
 // - [ ] 2개의 숫자에 대해 곱셈이 가능하다.
+
+it('2개의 숫자에 대해 곱셈이 가능하다', () => {
+    cy.visit('index.html');
+    cy.get(".digit").contains(2).click();
+    cy.get(".operation").contains("X").click();
+    cy.get(".digit").contains(9).click();
+    cy.get(".operation").contains("=").click();
+    cy.get('#total').should('have.text','18');
+});
+
 // - [ ] 2개의 숫자에 대해 나눗셈이 가능하다.
+
+it('2개의 숫자에 대해 나눗셈이 가능하다, 계산 결과를 표현할 때 소수점 이하는 버림한다.', () => {
+    cy.visit('index.html');
+    cy.get(".digit").contains(9).click();
+    cy.get(".operation").contains("/").click();
+    cy.get(".digit").contains(2).click();
+    cy.get(".operation").contains("=").click();
+    cy.get('#total').should('have.text','4');
+});
+
 // - [ ] AC(All Clear)버튼을 누르면 0으로 초기화 한다.
+
+it('AC(All Clear)버튼을 누르면 0으로 초기화 한다.', () => {
+    cy.visit('index.html');
+    cy.get(".digit").contains(8).click();
+    cy.get(".digit").contains(2).click();
+    cy.get(".modifier").contains("AC").click();
+    cy.get('#total').should('have.text','0');
+});
+
 // - [ ] 숫자는 한번에 최대 3자리 수까지 입력 가능하다.
-// - [ ] 계산 결과를 표현할 때 소수점 이하는 버림한다.
+
+it('숫자는 한번에 최대 3자리 수까지 입력 가능하다.', () => {
+    cy.visit('index.html');
+    cy.get(".digit").contains(8).click();
+    cy.get(".digit").contains(2).click();
+    cy.get(".digit").contains(4).click();
+    cy.get(".digit").contains(6).click();
+    cy.get('#total').should('have.text','824');
+});
+
+
