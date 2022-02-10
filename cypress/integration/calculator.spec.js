6ee67f091c2bf5ce788b8e783952284663b9b911
sangminsong <ssmin940606@gmail.com>
2022-02-10 11:20:17 +0900

    test: 곱셈 기능에 대한 테스트 코드 추가

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -1,5 +1,3 @@
-// - [ ] 2개의 숫자에 대해 뺄셈이 가능하다.
-// - [ ] 2개의 숫자에 대해 곱셈이 가능하다.
 // - [ ] 숫자는 한번에 최대 3자리 수까지 입력 가능하다.
 // - [ ] 계산 결과를 표현할 때 소수점 이하는 버림한다.
 
@@ -32,7 +30,6 @@ it('2개의 숫자에 대해 나눗셈이 가능하다.', () => {
   cy.get('#total').should('have.text', '2');
 });
 
-// - [ ] 2개의 숫자에 대해 뺄셈이 가능하다.
 it('2개의 숫자에 대해 뺄셈이 가능하다.', () => {
   // 초기화 한다.
   cy.get('.modifier').click();
@@ -46,6 +43,21 @@ it('2개의 숫자에 대해 뺄셈이 가능하다.', () => {
   cy.get('.operation').contains('=').click();
   // 2가 도출된다.
   cy.get('#total').should('have.text', '2');
+});
+
+it('2개의 숫자에 대해 곱셈이 가능하다.', () => {
+  // 초기화 한다.
+  cy.get('.modifier').click();
+  // 1을 누른다.
+  cy.get('.digit').contains(1).click();
+  // x을 누른다.
+  cy.get('.operation').contains('X').click();
+  // 7을 누른다.
+  cy.get('.digit').contains(7).click();
+  // =을 누른다.
+  cy.get('.operation').contains('=').click();
+  // 7이 도출된다.
+  cy.get('#total').should('have.text', '7');
 });
 
 it('AC(All Clear)버튼을 누르면 0으로 초기화 한다.', () => {
