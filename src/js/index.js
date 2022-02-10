1ca314763507f2182ae203bd5180b7cafe2ed0a9
Hwang Jung Min <hwangstar156@gmail.com>
2022-02-10 10:54:13 +0900

    chore: prettier 설정

--- src/js/index.js
+++ src/js/index.js
@@ -1,127 +1,140 @@
-import {DIGITS_LIMIT,INIT_NUMBER,OPERATORS,alertMessage,CLASSES,ID,VALID_INPUT_NUMBERS_LENGTH,OPERATORS_REGEX} from "./utils/constants.js"
-import {$} from './utils/selector.js'
+import {
+  DIGITS_LIMIT,
+  INIT_NUMBER,
+  OPERATORS,
+  alertMessage,
+  CLASSES,
+  ID,
+  VALID_INPUT_NUMBERS_LENGTH,
+  OPERATORS_REGEX,
+} from './utils/constants.js';
+import {$} from './utils/selector.js';
 
 class Calculator {
-    constructor() {
-        this.isStarted = false;
-        this.currentNumber = '';
-        this.inputNumbers = [];
-        this.currentOpertor = ''
-    }
+  constructor() {
+    this.isStarted = false;
+    this.currentNumber = '';
+    this.inputNumbers = [];
+    this.currentOpertor = '';
+  }
 
-    init = () => {
-        this.bindDOMs();
-        this.bindEvent();
-    }
+  init = () => {
+    this.bindDOMs();
+    this.bindEvent();
+  };
 
-    bindEvent = () => {
-        $(CLASSES.DIGITS).addEventListener("click", this.clickDigitHandler);
-        $(CLASSES.OPERATIONS).addEventListener("click", this.clickOperatorHandler);
-        $(CLASSES.MODIFIER).addEventListener("click", this.clickModifierHandler);
-    }
+  bindEvent = () => {
+    $(CLASSES.DIGITS).addEventListener('click', this.clickDigitHandler);
+    $(CLASSES.OPERATIONS).addEventListener('click', this.clickOperatorHandler);
+    $(CLASSES.MODIFIER).addEventListener('click', this.clickModifierHandler);
+  };
 
-    bindDOMs = () => {
-        this.$totalResult = document.querySelector(ID.TOTAL);
-    }
-    
-    clickModifierHandler = () => {
-        this.isStarted = false;
-        this.currentNumber = '';
-        this.inputNumbers = [];
-        this.currentOpertor = '';
-        this.renderResult(INIT_NUMBER);
-    }
+  bindDOMs = () => {
+    this.$totalResult = document.querySelector(ID.TOTAL);
+  };
 
-    clickDigitHandler = (e) => {
-        if (this.isOverThreeDigit()) {
-            alert(alertMessage.OVER_THREE_DIGIT);
-            return;
-        }
-        const selectedNumber = e.target.innerText;
-        this.currentNumber += selectedNumber;
-        this.renderDigit(selectedNumber);
-    }
+  clickModifierHandler = () => {
+    this.isStarted = false;
+    this.currentNumber = '';
+    this.inputNumbers = [];
+    this.currentOpertor = '';
+    this.renderResult(INIT_NUMBER);
+  };
 
-    renderDigit = (digit) => {
-        if(!this.isStarted){
-            this.isStarted = true;
-            this.$totalResult.innerText = '';
-        }   
-        this.$totalResult.innerText += digit;
+  clickDigitHandler = e => {
+    if (this.isOverThreeDigit()) {
+      alert(alertMessage.OVER_THREE_DIGIT);
+      return;
     }
+    const selectedNumber = e.target.innerText;
+    this.currentNumber += selectedNumber;
+    this.renderDigit(selectedNumber);
+  };
 
-    renderOperator = (operator) => {
-        this.$totalResult.innerText += operator;
+  renderDigit = digit => {
+    if (!this.isStarted) {
+      this.isStarted = true;
+      this.$totalResult.innerText = '';
     }
+    this.$totalResult.innerText += digit;
+  };
 
-    renderResult = (result) => {
-        this.$totalResult.innerText = result;
-    }
+  renderOperator = operator => {
+    this.$totalResult.innerText += operator;
+  };
 
-    isOverThreeDigit = () => {
-        return this.currentNumber.length >= DIGITS_LIMIT;
-    }
+  renderResult = result => {
+    this.$totalResult.innerText = result;
+  };
 
-    clickOperatorHandler = (e) => {
-        const selectedOperator = e.target.innerText;
-        if(!this.checkValidOperatorInput(selectedOperator)){
-            return;
-        }
-        this.inputNumbers.push(Number(this.currentNumber));
-        this.currentNumber = "";
-        if(this.isEqualOperator(selectedOperator)){
-            this.renderResult(this.calculate());
-            return;
-        }
-        this.currentOpertor = selectedOperator;
-        this.renderOperator(selectedOperator);
-    }
+  isOverThreeDigit = () => {
+    return this.currentNumber.length >= DIGITS_LIMIT;
+  };
 
-    checkValidOperatorInput = (operator) => {
-        if(!this.isEqualOperator(operator) && this.currentOpertor){
-            alert(alertMessage.OVER_TWO_NUMBER);
-            return false;
-        }
-        if(this.isDuplicatedOperator()){
-            alert(alertMessage.DUPLICATED_OPERATOR);
-            return false;
-        }
-        if(this.isEqualOperator(operator) && this.invalidEqualOperatorHandler()){
-            return false;
-        }
-        return true;
+  clickOperatorHandler = e => {
+    const selectedOperator = e.target.innerText;
+    if (!this.checkValidOperatorInput(selectedOperator)) {
+      return;
     }
-    
-    calculate = () => {
-        const [first,second] = this.inputNumbers;
-        switch (this.currentOpertor) {
-            case OPERATORS.PLUS:
-                return first + second;
-            case OPERATORS.MULTIPLY:
-                return first * second;
-            case OPERATORS.MINUS:
-                return first - second;
-            case OPERATORS.DIVISION:
-                return Math.floor(first/second);
-            default:
-                return;
-        }
+    this.inputNumbers.push(Number(this.currentNumber));
+    this.currentNumber = '';
+    if (this.isEqualOperator(selectedOperator)) {
+      this.renderResult(this.calculate());
+      return;
     }
+    this.currentOpertor = selectedOperator;
+    this.renderOperator(selectedOperator);
+  };
 
-    isEqualOperator = (operator) => operator === OPERATORS.EQUAL;
-
-    invalidEqualOperatorHandler = () => {
-        if(!this.currentOpertor || this.inputNumbers.length < VALID_INPUT_NUMBERS_LENGTH){
-            alert(alertMessage.WRONG_EQUAL_INPUT)
-            return true;
-        }
-        return false;
+  checkValidOperatorInput = operator => {
+    if (!this.isEqualOperator(operator) && this.currentOpertor) {
+      alert(alertMessage.OVER_TWO_NUMBER);
+      return false;
     }
-    
-    isDuplicatedOperator = () => {
-        return this.$totalResult.innerText[this.$totalResult.innerText.length - 1].match(OPERATORS_REGEX);
+    if (this.isDuplicatedOperator()) {
+      alert(alertMessage.DUPLICATED_OPERATOR);
+      return false;
     }
+    if (this.isEqualOperator(operator) && this.invalidEqualOperatorHandler()) {
+      return false;
+    }
+    return true;
+  };
 
+  calculate = () => {
+    const [first, second] = this.inputNumbers;
+    switch (this.currentOpertor) {
+      case OPERATORS.PLUS:
+        return first + second;
+      case OPERATORS.MULTIPLY:
+        return first * second;
+      case OPERATORS.MINUS:
+        return first - second;
+      case OPERATORS.DIVISION:
+        return Math.floor(first / second);
+      default:
+        return;
+    }
+  };
+
+  isEqualOperator = operator => operator === OPERATORS.EQUAL;
+
+  invalidEqualOperatorHandler = () => {
+    if (
+      !this.currentOpertor ||
+      this.inputNumbers.length < VALID_INPUT_NUMBERS_LENGTH
+    ) {
+      alert(alertMessage.WRONG_EQUAL_INPUT);
+      return true;
+    }
+    return false;
+  };
+
+  isDuplicatedOperator = () => {
+    return this.$totalResult.innerText[
+      this.$totalResult.innerText.length - 1
+    ].match(OPERATORS_REGEX);
+  };
 }
 
 const calculator = new Calculator();
