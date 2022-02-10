32d9ec6983a6d609c0f1fe440bc48b346af5387f
sangminsong <ssmin940606@gmail.com>
2022-02-10 11:27:59 +0900

    test: 예외사항에 대한 테스트 코드 추가 -계산 결과를 표현할 때 소수점 이하는 버림한다.

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -1,5 +1,4 @@
 // - [ ] 숫자는 한번에 최대 3자리 수까지 입력 가능하다.
-// - [ ] 계산 결과를 표현할 때 소수점 이하는 버림한다.
 
 it('2개의 숫자에 대해 덧셈이 가능하다.', () => {
   cy.visit('index.html');
@@ -13,21 +12,6 @@ it('2개의 숫자에 대해 덧셈이 가능하다.', () => {
   cy.get('.operation').contains('=').click();
   // 9가 도출된다.
   cy.get('#total').should('have.text', '9');
-});
-
-it('2개의 숫자에 대해 나눗셈이 가능하다.', () => {
-  // 초기화 한다.
-  cy.get('.modifier').click();
-  // 6을 누른다.
-  cy.get('.digit').contains(6).click();
-  // /를 누른다.
-  cy.get('.operation').contains('/').click();
-  // 3을 누른다.
-  cy.get('.digit').contains(3).click();
-  // =을 누른다.
-  cy.get('.operation').contains('=').click();
-  // 2가 도출된다.
-  cy.get('#total').should('have.text', '2');
 });
 
 it('2개의 숫자에 대해 뺄셈이 가능하다.', () => {
@@ -60,9 +44,39 @@ it('2개의 숫자에 대해 곱셈이 가능하다.', () => {
   cy.get('#total').should('have.text', '7');
 });
 
+it('2개의 숫자에 대해 나눗셈이 가능하다.', () => {
+  // 초기화 한다.
+  cy.get('.modifier').click();
+  // 6을 누른다.
+  cy.get('.digit').contains(6).click();
+  // /를 누른다.
+  cy.get('.operation').contains('/').click();
+  // 3을 누른다.
+  cy.get('.digit').contains(3).click();
+  // =을 누른다.
+  cy.get('.operation').contains('=').click();
+  // 2가 도출된다.
+  cy.get('#total').should('have.text', '2');
+});
+
 it('AC(All Clear)버튼을 누르면 0으로 초기화 한다.', () => {
   // AC를 누른다.
   cy.get('.modifier').click();
   // 0이 도출된다.
   cy.get('#total').should('have.text', '0');
+});
+
+it('계산 결과를 표현할 때 소수점 이하는 버림한다.', () => {
+  // 초기화 한다.
+  cy.get('.modifier').click();
+  // 7을 누른다.
+  cy.get('.digit').contains(7).click();
+  // /를 누른다.
+  cy.get('.operation').contains('/').click();
+  // 3을 누른다.
+  cy.get('.digit').contains(3).click();
+  // =을 누른다.
+  cy.get('.operation').contains('=').click();
+  // 2가 도출된다.
+  cy.get('#total').should('have.text', '2');
 });
