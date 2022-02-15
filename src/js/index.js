2aa2a84f3d83556d554720fc80987c50b0be8a4a
airman5573 <68623798+airman5573@users.noreply.github.com>
2022-02-15 15:41:37 +0900

    [병민-안] 계산기 미션 제출합니다. (#53)
    * :truck: chore(commitlint): commitlint를 추가한다
    
    commit을 날릴때 일정한 양식을 강제하는 기능을 추가한다.
    
    * :memo: docs(readme.md): 구현해야할 기능 목록을 추가한다
    
    * :sparkles: feat(utils): querySelector를 짧게 쓸 수 있도록 $함수를 추가한다
    
    * :sparkles: feat(.gitignore): .vscode폴더는 tracking하지 않는다
    
    * :recycle: refactor(utils): $함수를 기본으로 export한다
    
    * :sparkles: feat(calculator): 기본적인 사칙연산을 수행한다
    
    * :sparkles: feat(calculator): 연산과정을 View에 반영한다
    
    * :sparkles: feat(calculator): 피연산자의 자리수에 대해 유효성 검사를 한다
    
    피연산자의 자리수는 3자리를 넘으면 안된다.
    
    * :sparkles: feat(calculator): all clear기능을 추가한다
    
    ac(all clear)버튼을 눌렀을때 모든 상태값을 초기화 한다
    
    * :bug: fix(constants): initial_number의 타입을 변경한다
    
    * :sparkles: feat(calculator): 결과값이 나온 이후에 연속적으로 계산을 할 수 있다
    
    * :recycle: refactor(calculator): 연산을 하기에 피연산자가 충분한지 유효성 검사를 한다
    
    +,-,*,/ 같은 경우 왼쪽에 피연산자가 있어야 하며, =같은 경우는 좌우에 피연산자가 둘다 있어야 한다.
    
    * :recycle: refactor(calculator): 연산의 결과가 나온 이후에 숫자를 누르면 초기화한다
    
    * :recycle: refactor(calculator): 피연산자의 개수를 제한한다
    
    피연산자는 최대 2개까지만 허용된다
    
    * :recycle: refactor(calculator): 0으로 나누는 경우를 제한한다
    
    * :memo: docs(readme.md): 유효성 검사 항목을 추가한다
    
    * :recycle: refactor(calculator): binding하지 않고 바로 Element를 찾아 사용한다
    
    자주 사용하지 않기 때문에, 미리 binding해놓는 의미가 없다. 코드만 길어질뿐.
    
    * :recycle: refactor(calculator): 연속적인 연산자 입력을 제한한다
    
    * :white_check_mark: test(test): test코드를 추가한다
    
    * :recycle: refactor(readme.md): docs파일 위치를 변경한다

---
+++ src/js/index.js
@@ -0,0 +1,156 @@
+import $ from "./utils/dom.js";
+import arrayToNumber from "./utils/arrayToNumber.js";
+import { SELECTORS, OPERATIONS, INITIAL_NUMBER, MAX_DIGIT_SIZE, ERROR_MESSAGES } from "./constants.js";
+
+class Calculator {
+  constructor() {
+    document.addEventListener('DOMContentLoaded', () => {
+      this.addEventListeners();
+      this.initState();
+    });
+  }
+
+  addEventListeners() {
+    $(SELECTORS.digits).addEventListener('click', (e) => {
+      if (this.calculatedResult !== 0) {
+        this.reset();
+      }
+
+      const digit = parseInt(e.target.innerText, 10);
+
+      if (!this.currentOperation && this.isOverMaxDigitSize(this.firstNumberArray)) {
+        alert(ERROR_MESSAGES.underMaxDigitSize);
+        return;
+      }
+
+      if (!this.currentOperation) {
+        this.firstNumberArray.push(digit);
+        this.renderTotal(this.firstNumberArray.join(''));
+        return;
+      }
+
+      if (this.isOverMaxDigitSize(this.secondNumberArray)) {
+        alert(ERROR_MESSAGES.underMaxDigitSize);
+        return;
+      }
+
+      this.secondNumberArray.push(digit);
+      this.renderTotal(this.secondNumberArray.join(''));
+    });
+
+    $(SELECTORS.operations).addEventListener('click', (e) => {
+      const operation = e.target.getAttribute('data-operation');
+
+      // 이전의 결과 값이 남아 있는데, +,- 같은 연산을 하는 경우
+      if (this.calculatedResult) {
+        // firstNumArray에 이전 결과값을 채운다
+        this.firstNumberArray = `${this.calculatedResult}`.split('').map((numStr) => Number(numStr));
+        this.secondNumberArray = [];
+        this.calculatedResult = 0;
+      }
+
+      // validation
+      // =인데, 피연산자가 하나라도 없다면 ERROR
+      if ((operation === OPERATIONS.equal) && (this.firstNumberArray.length === 0 || this.secondNumberArray.length === 0)) {
+        alert(ERROR_MESSAGES.operationBetweenNumber);
+        return;
+      }
+
+      // =이 아닌데(+, -, *, /) 첫번째 피연산자가 없다면
+      if (operation !== OPERATIONS.equal && (this.firstNumberArray.length === 0)) {
+        alert(ERROR_MESSAGES.operationBetweenNumber);
+        return;
+      }
+
+      // =이 아닌데(+, -, *, /) 피연산자가 2개 꽉 차있다면
+      if (operation !== OPERATIONS.equal && (this.firstNumberArray.length > 0 && this.secondNumberArray.length > 0)) {
+        alert(ERROR_MESSAGES.onlyTwoOperlands);
+        return;
+      }
+
+      // + -> + / + -> - / + -> =
+      if (operation !== OPERATIONS.equal && this.currentOperation) {
+        alert(ERROR_MESSAGES.operationBetweenNumber);
+        this.currentOperation = '';
+        return;
+      }
+
+      if (operation === OPERATIONS.equal) {
+        const firstNum = arrayToNumber(this.firstNumberArray);
+        const secondNum = arrayToNumber(this.secondNumberArray);
+        const result = this.calculate(firstNum, secondNum);
+        // 에러가 발생해서 null을 리턴한 경우, 계산기를 초기화 해버린다
+        if (result === null) {
+          this.reset();
+          return;
+        }
+        this.calculatedResult = result;
+        this.renderTotal(`${this.calculatedResult}`);
+        this.currentOperation = '';
+        return;
+      }
+      this.currentOperation = operation;
+    });
+
+    $(SELECTORS.modifier).addEventListener('click', () => {
+      this.reset();
+    });
+  }
+
+  isOverMaxDigitSize(numArr) {
+    return numArr.length >= MAX_DIGIT_SIZE;
+  }
+
+  initState() {
+    this.currentOperation = '';
+    this.firstNumberArray = [];
+    this.secondNumberArray = [];
+    this.calculatedResult = INITIAL_NUMBER;
+    this.renderTotal(this.calculatedResult);
+  }
+
+  reset() {
+    this.initState();
+  }
+
+  renderTotal(result) {
+    $(SELECTORS.total).innerText = result;
+  }
+
+  calculate(firstNum, secondNum) {
+    if (this.currentOperation === OPERATIONS.plus) {
+      return this.add(firstNum, secondNum);
+    }
+    if (this.currentOperation === OPERATIONS.minus) {
+      return this.minus(firstNum, secondNum);
+    }
+    if(this.currentOperation === OPERATIONS.multiply) {
+      return this.multiply(firstNum, secondNum);
+    }
+    if (this.currentOperation === OPERATIONS.divide) {
+      return this.divide(firstNum, secondNum);
+    }
+  }
+
+  add(firstNum, secondNum) {
+    return firstNum + secondNum;
+  }
+
+  minus(firstNum, secondNum) {
+    return firstNum - secondNum;
+  }
+
+  multiply(firstNum, secondNum) {
+    return firstNum * secondNum;
+  }
+
+  divide(firstNum, secondNum) {
+    if (secondNum === 0) {
+      alert(ERROR_MESSAGES.divideWithPositiveNumber);
+      return null;
+    }
+    return Math.floor(firstNum / secondNum);
+  }
+}
+
+new Calculator();
---
+++ src/js/utils/arrayToNumber.js
@@ -0,0 +1,3 @@
+export default function arrayToNumber(numberArr) {
+  return Number(numberArr.join(''));
+}
