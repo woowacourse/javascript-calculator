983ae5025ef67609d34b6a0c6c29c776f087126d
Kimsanglok <evertree6031@gmail.com>
2022-02-15 16:22:08 +0900

    [빅터-록바] 계산기 미션 제출합니다. (#21)
    * Docs: 기능 요구사항 작성
    
    * Chore: eslint, prettier, cypress 설정
    
    * Test: 기능 요구사항에 맞는 테스트 케이스 작성
    
    * Feat: body 태그 하단에 script 태그 추가
    
    * Feat: dom 객체를 멤버 변수로 초기화
    
    * Feat: 이벤트 위임을 적용한 digits 이벤트 핸들러 등록
    
    * Feat: 이벤트 위임을 적용한 operations 이벤트 핸들러 등록
    
    * Feat: modifier에 이벤트 핸들러 등록
    
    * Test: visit 커맨드의  url 수정
    
    * Feat: 계산기 연산기능 - 공통 예외사항 처리
    
    * Refactor: 선택자, 에러 메시지, 연산자 상수화 적용
    
    * Refactor: 함수가 하나의 역할만 수행하도록 추상화 진행 및 업그레이드
    
    Co-authored-by: woose28 <dldntmdsla@naver.com>

---
+++ src/js/constant/index.js
@@ -0,0 +1,37 @@
+export const ID = Object.freeze({
+  TOTAL: 'total',
+});
+
+export const CLASS = Object.freeze({
+  DIGITS: 'digits',
+  MODIFIER: 'modifier',
+  OPERATIONS: 'operations',
+  DIGIT: 'digit',
+  OPERATION: 'operation',
+});
+
+export const ERROR_MESSAGES = Object.freeze({
+  EXCEED_THREE_LENGTH: '숫자는 세 자리를 초과하면 안됩니다.',
+  ENTER_TWO_NUMBERS: '숫자는 두 개를 입력하셔야 합니다.',
+  DIVIDED_NOT_ZERO: '0으로 나눌 수 없습니다.',
+  EXIST_OPERAND: '연산자는 하나만 입력해주세요.',
+});
+
+export const OPERATORS = Object.freeze({
+  ADD: '+',
+  SUBSTRACT: '-',
+  MULTIPLY: 'X',
+  DIVIDE: '/',
+  EQUAL: '=',
+});
+
+export const RULES = Object.freeze({
+  MAX_OPERAND_LENGTH: 3,
+  MAX_OPERAND_NUMBER: 2,
+  INITIAL_VALUE: '',
+  ZERO_NUMBER: 0,
+  NOTHING: null,
+});
+
+export const FIRST_INDEX = 0;
+export const SECOND_INDEX = 1;
